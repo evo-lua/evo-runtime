741fe3558c515f57e48c16799463752106eb90e1
RDW <duckwhale@mail.ru>
2023-01-06 10:57:34 +0100

    Tests: Add a basic smoke test for the new runtime

---
+++ Tests/smoke-test.lua
@@ -0,0 +1,22 @@
+-- If there's an issue with the native bootstrapping code that initializes the Lua environment, all bets are off
+print()
+print("Running basic smoke tests ...")
+print()
+
+local assertions = {
+	{
+		actual = arg[0],
+		expected = "Tests/smoke-test.lua",
+		description = "The global arg table should contain the script name at index 0",
+	},
+}
+
+for _, assertionInfo in ipairs(assertions) do
+	assert(assertionInfo.actual == assertionInfo.expected, assertionInfo.description)
+	print("OK", assertionInfo.description)
+end
+
+print()
+print("Good news, everyone! There's at least a chance that the runtime isn't completely broken - time to celebrate:")
+print()
+print("(>'-')> <('-'<) ^('-')^ v('-')v(>'-')> (^-^)")
