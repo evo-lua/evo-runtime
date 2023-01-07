f685fa1611d288c6293a7ee316468e4f7b29c58c
RDW <duckwhale@mail.ru>
2023-01-07 08:17:09 +0100

    Deps: Temporarily disable building missing dependencies

--- Dependencies/unixbuild-all.sh
+++ Dependencies/unixbuild-all.sh
@@ -1,7 +1,7 @@
 Dependencies/luajit-unixbuild.sh
-Dependencies/llhttp-unixbuild.sh
-Dependencies/openssl-unixbuild.sh
-Dependencies/luaopenssl-unixbuild.sh
-Dependencies/luv-unixbuild.sh
-Dependencies/zlib-unixbuild.sh
-Dependencies/pcre-unixbuild.sh
+# Dependencies/llhttp-unixbuild.sh
+# Dependencies/openssl-unixbuild.sh
+# Dependencies/luaopenssl-unixbuild.sh
+# Dependencies/luv-unixbuild.sh
+# Dependencies/zlib-unixbuild.sh
+# Dependencies/pcre-unixbuild.sh
--- Dependencies/windowsbuild-all.sh
+++ Dependencies/windowsbuild-all.sh
@@ -1,7 +1,7 @@
 Dependencies/luajit-windowsbuild.sh
-Dependencies/llhttp-windowsbuild.sh
-Dependencies/openssl-windowsbuild.sh
-Dependencies/luaopenssl-windowsbuild.sh
-Dependencies/luv-windowsbuild.sh
-Dependencies/zlib-windowsbuild.sh
-Dependencies/pcre-windowsbuild.sh
+# Dependencies/llhttp-windowsbuild.sh
+# Dependencies/openssl-windowsbuild.sh
+# Dependencies/luaopenssl-windowsbuild.sh
+# Dependencies/luv-windowsbuild.sh
+# Dependencies/zlib-windowsbuild.sh
+# Dependencies/pcre-windowsbuild.sh
