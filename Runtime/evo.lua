2c4fa5dc02ce788b6343e4d54ed4f61a6e11ab1f
RDW <duckwhale@mail.ru>
2023-01-18 13:34:08 +0100

    Runtime: Create a global alias for the Lua 5.2 bit32 library
    This is assumed to exist by diff-match-patch, and since the repository seems abandoned a fix cannot easily be pushed to upstream.
    
    That's obviously not an ideal solution, but since there will be more aliases (printf, dump, etc.) later, adding a standard location for them is likely a good idea.

--- Runtime/evo.lua
+++ Runtime/evo.lua
@@ -1,12 +1,18 @@
 local evo = {}
 
 function evo.run()
+	evo.setupGlobalEnvironment()
 	evo.loadNonstandardExtensions()
 
 	print("Hello from evo.lua!")
 
 	local scriptFile = arg[0]
 	dofile(scriptFile)
+end
+
+function evo.setupGlobalEnvironment()
+	-- Lua 5.2 compatibility (required for the diff-match-patch library)
+	_G.bit32 = require("bit")
 end
 
 function evo.loadNonstandardExtensions()
