d660c53a6d89476dd50f5d66dfad53b59d50844d
RDW <duckwhale@mail.ru>
2023-02-05 15:09:43 +0100

    Update lws-windowsbuild.sh

--- deps/lws-windowsbuild.sh
+++ deps/lws-windowsbuild.sh
@@ -4,8 +4,8 @@ SRC_DIR=deps/warmcat/libwebsockets
 BUILD_DIR=$SRC_DIR/cmakebuild-windows
 OUT_DIR=ninjabuild-windows
 
-# TBD: WITH_OPENSSL, libuv, zlib/miniz?
-cmake -S $SRC_DIR -B $BUILD_DIR -G Ninja -DBUILD_MODULE=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC_LIBS=ON -DCMAKE_C_COMPILER=gcc
+# TBD: WITH_OPENSSL, libuv, zlib/miniz? uses system openssl by default, should use deps/openssl one...
+cmake -S $SRC_DIR -B $BUILD_DIR -G Ninja -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_COMPILER=gcc
 cmake --build $BUILD_DIR --clean-first
 
 # cp $BUILD_DIR/libluv.a $OUT_DIR
