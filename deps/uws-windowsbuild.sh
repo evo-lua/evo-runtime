cf37f65ccef524d55c5c07ec1c9dbf97a48c052d
RDW <duckwhale@mail.ru>
2023-02-05 17:31:26 +0100

    Update uws-windowsbuild.sh

--- deps/uws-windowsbuild.sh
+++ deps/uws-windowsbuild.sh
@@ -4,11 +4,30 @@ UWS_DIR=deps/uNetworking/uWebSockets
 UWS_SOURCE_DIR=$UWS_DIR/src
 BUILD_DIR=ninjabuild-windows
 
+# uws doesn't have a build system, and it builds usockets with options we don't want (and can't remove...)
+# So we build it manually and copy that version, which should be fine since uws itself doesn't use these options
+cd $UWS_DIR/uSockets
+make LIBUS_USE_OPENSSL=0 ITH_LTO=0
+cp uSockets.a $BUILD_DIR
+cd -
+
 cd $UWS_DIR/capi
 
 # make clean
 # TBD: WITH_OPENSSL? WITH_QUIC? WITH_LIBUV=1 ? also DISABLE these: LIBUS_NO_SSL  UWS_NO_ZLIB - and use the libuv/zlib we built
-make capi
+# export WITH_ZLIB=1
+# export WITH_LIBUV=1
+# export LIBUS_NO_SSL=1
+set LIBUS_USE_OPENSSL=0
+export LIBUS_USE_OPENSSL=0
+# export UWS_NO_ZLIB=1
+# WITH_LTO doesn't work on Windows
+# WITH_LIBUV=1 WITH_LTO=0
+# make capi
+# todo move to ninjabuild I guess
+LIBRARY_NAME=libuwebsockets
+g++ -DUWS_WITH_PROXY -c -O3 -std=c++17 -lz -luv -flto -fPIC -I ../src -I ../uSockets/src $LIBRARY_NAME.cpp
+ar rvs $LIBRARY_NAME.a $LIBRARY_NAME.o ../uSockets/uSockets.a
 
 cd -
 
@@ -19,4 +38,4 @@ cd -
 #$BUILD_DIR/luajit.exe -e "print(\"Hello from LuaJIT! (This is a test and can be ignored)\")"
 
 # This is needed to save bytecode via luajit -b since the jit module isn't embedded inside the executable
-#cp $UWS_SOURCE_DIR/jit/* $BUILD_DIR/jit
+cp $UWS_DIR/capi/libuwebsockets.a $BUILD_DIR
