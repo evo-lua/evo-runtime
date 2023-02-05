939fb32e592fdb9890c43aa72cf09ed039ec48fd
RDW <duckwhale@mail.ru>
2023-02-05 15:27:22 +0100

    Update lws-windowsbuild.sh

--- deps/lws-windowsbuild.sh
+++ deps/lws-windowsbuild.sh
@@ -9,9 +9,10 @@ mkdir -p $OUT_DIR/libwebsockets
 # Their clean target doesn't seem to be working with ninja because there may be leftover files...
 rm -rf $BUILD_DIR
 
+# Disable examples, no need to build them here
 # TBD: WITH_OPENSSL, libuv, zlib/miniz? uses system openssl by default, should use deps/openssl one...
-cmake -S $SRC_DIR -B $BUILD_DIR -G Ninja -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_COMPILER=gcc
+cmake -S $SRC_DIR -B $BUILD_DIR -G Ninja -DBUILD_SHARED_LIBS=OFF -DLWS_WITH_MINIMAL_EXAMPLES=OFF -DCMAKE_C_COMPILER=gcc
 cmake --build $BUILD_DIR --clean-first
 
 cp $BUILD_DIR/lib/libwebsockets_static.a $OUT_DIR
-cp $BUILD_DIR/include/* $OUT_DIR
+cp -r $BUILD_DIR/include/* $OUT_DIR
