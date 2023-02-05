634739e47a7fa18fbaa6fb2cc87c70aecc3d0cfe
RDW <duckwhale@mail.ru>
2023-02-05 18:21:31 +0100

    Update uws-windowsbuild.sh

--- deps/uws-windowsbuild.sh
+++ deps/uws-windowsbuild.sh
@@ -3,12 +3,15 @@ echo Building target uwebsockets
 UWS_DIR=deps/uNetworking/uWebSockets
 UWS_SOURCE_DIR=$UWS_DIR/src
 BUILD_DIR=ninjabuild-windows
+ABS_BUILD_DIR=$(pwd)/$BUILD_DIR
 
 # uws doesn't have a build system, and it builds usockets with options we don't want (and can't remove...)
 # So we build it manually and copy that version, which should be fine since uws itself doesn't use these options
 cd $UWS_DIR/uSockets
-make LIBUS_USE_OPENSSL=0 ITH_LTO=0
-cp uSockets.a $BUILD_DIR
+make
+cp uSockets.a $ABS_BUILD_DIR
+# ls
+# exit 42
 cd -
 
 cd $UWS_DIR/capi
@@ -18,16 +21,16 @@ cd $UWS_DIR/capi
 # export WITH_ZLIB=1
 # export WITH_LIBUV=1
 # export LIBUS_NO_SSL=1
-set LIBUS_USE_OPENSSL=0
-export LIBUS_USE_OPENSSL=0
+# set LIBUS_USE_OPENSSL=0
+# export WITH_OPENSSL=0
 # export UWS_NO_ZLIB=1
 # WITH_LTO doesn't work on Windows
 # WITH_LIBUV=1 WITH_LTO=0
 # make capi
 # todo move to ninjabuild I guess
 LIBRARY_NAME=libuwebsockets
-g++ -DUWS_WITH_PROXY -c -O3 -std=c++17 -lz -luv -flto -fPIC -I ../src -I ../uSockets/src $LIBRARY_NAME.cpp
-ar rvs $LIBRARY_NAME.a $LIBRARY_NAME.o ../uSockets/uSockets.a
+g++ -DUWS_WITH_PROXY -DUWS_NO_ZLIB -DLIBUS_NO_SSL -c -O3 -std=c++17 -lz -luv -fPIC -I ../src -I ../uSockets/src $LIBRARY_NAME.cpp
+ar rvs $LIBRARY_NAME.a $LIBRARY_NAME.o $ABS_BUILD_DIR/uSockets.a
 
 cd -
 
