846b4c15a8af20df4b15ab3992d10a9e3b89ea75
RDW <duckwhale@mail.ru>
2023-02-05 14:17:05 +0100

    Update ninjabuild.lua

--- ninjabuild.lua
+++ ninjabuild.lua
@@ -37,6 +37,7 @@ local EvoBuildTarget = {
 	cppSources = {
 		"Runtime/main.cpp",
 		"Runtime/evo.cpp",
+		"Runtime/Bindings/uws.cpp",
 		"Runtime/Bindings/webview_ffi.cpp",
 		"Runtime/LuaVirtualMachine.cpp",
 	},
