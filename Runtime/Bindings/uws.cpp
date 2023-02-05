f72e56d4117476b111a2c2b853d20a2a4f079ce9
RDW <duckwhale@mail.ru>
2023-02-05 14:34:29 +0100

    Update uws.cpp

--- Runtime/Bindings/uws.cpp
+++ Runtime/Bindings/uws.cpp
@@ -1,82 +1,52 @@
-// #include <iostream>
-// #include <uv.h>
-// #include <App.h>
+/* We simply call the root header file "App.h", giving you uWS::App and uWS::SSLApp */
+#include "App.h"
 
-// class MyServer {
-// public:
-// 	MyServer()
-// 		: app(uWS::App().get()) {
-// 		app->get("/", [](uWS::HttpResponse* res, uWS::HttpRequest req, char* data, size_t length, size_t remainingBytes) {
-// 			res->end("Hello from uWebSockets!");
-// 		});
-
-// 		app->ws<MyServer>("/*", {
-// 									/* Settings */
-// 									.compression = uWS::SHARED_COMPRESSOR,
-// 									.maxPayloadLength = 16 * 1024,
-// 									.idleTimeout = 10,
-// 									/* Handlers */
-// 									.open = &MyServer::onConnection,
-// 									.message = &MyServer::onMessage,
-// 									.drain = &MyServer::onDrain,
-// 									.close = &MyServer::onDisconnection,
-// 								});
-// 	}
-
-// 	void onConnection(uWS::WebSocket<uWS::SERVER>* ws, uWS::HttpRequest req) {
-// 		std::cout << "WebSocket connection established" << std::endl;
-// 	}
-
-// 	void onMessage(uWS::WebSocket<uWS::SERVER>* ws, char* message, size_t length, uWS::OpCode opCode) {
-// 		std::cout << "Received message: " << std::string(message, length) << std::endl;
-// 		ws->send(message, length, opCode);
-// 	}
-
-// 	void onDrain(uWS::WebSocket<uWS::SERVER>* ws) {
-// 		std::cout << "WebSocket send buffer drained" << std::endl;
-// 	}
-
-// 	void onDisconnection(uWS::WebSocket<uWS::SERVER>* ws, int code, char* message, size_t length) {
-// 		std::cout << "WebSocket connection closed with code " << code << " and message: " << std::string(message, length) << std::endl;
-// 	}
-
-// 	void run(const char* hostname, int port) {
-// 		uv_loop_t* loop = uv_default_loop();
-
-// 		// Register the WebSocket server to the libuv event loop
-// 		uv_idle_init(loop, &idler);
-// 		uv_idle_start(&idler, [](uv_idle_t* handle) {
-// 			auto server = reinterpret_cast<MyServer*>(handle->data);
-// 			server->app->poll();
-// 		});
-// 		idler.data = this;
-
-// 		// Start the WebSocket server
-// 		app->listen(hostname, port, [this](uWS::WebSocket<uWS::SERVER>* ws) {
-// 			   ws->setData(this);
-// 		   })
-// 			.run();
-// 	}
-
-#include <uWS/uWS.h>
-#include <iostream>
-#include <string>
-
-std::vector<std::string> messages;
-
-void onMessage(uWS::WebSocket<uWS::SERVER> *ws, char *message, size_t length, uWS::OpCode opCode) {
-    messages.push_back(std::string(message, length));
-}
-
-void onConnection(uWS::WebSocket<uWS::SERVER> *ws, uWS::UpgradeInfo *upgradeInfo) {
-    ws->send("Welcome to the WebSocket server!", opCodeBinary);
-}
+/* This is a simple WebSocket echo server example.
+ * You may compile it with "WITH_OPENSSL=1 make" or with "make" */
 
 int uws_test() {
-    uWS::Hub h;
-    h.onMessage(onMessage);
-    h.onConnection(onConnection);
-    h.listen(3000);
-    h.run();
-    return 0;
+	/* ws->getUserData returns one of these */
+	struct PerSocketData {
+		/* Fill with user data */
+	};
+
+	/* Keep in mind that uWS::SSLApp({options}) is the same as uWS::App() when compiled without SSL support.
+     * You may swap to using uWS:App() if you don't need SSL */
+	uWS::App(
+		// {
+		/* There are example certificates in uWebSockets.js repo */
+		// .key_file_name = "misc/key.pem",
+		// .cert_file_name = "misc/cert.pem",
+		// .passphrase = "1234"
+		// }
+		)
+		.ws<PerSocketData>("/*", { /* Settings */
+									 .compression = uWS::CompressOptions(uWS::DEDICATED_COMPRESSOR_4KB | uWS::DEDICATED_DECOMPRESSOR),
+									 .maxPayloadLength = 100 * 1024 * 1024,
+									 .idleTimeout = 16,
+									 .maxBackpressure = 100 * 1024 * 1024,
+									 .closeOnBackpressureLimit = false,
+									 .resetIdleTimeoutOnSend = false,
+									 .sendPingsAutomatically = true,
+									 /* Handlers */
+									 .upgrade = nullptr,
+									 .open = [](auto* /*ws*/) {
+										 /* Open event here, you may access ws->getUserData() which points to a PerSocketData struct */ },
+									 .message = [](auto* ws, std::string_view message, uWS::OpCode opCode) { ws->send(message, opCode, true); },
+									 .drain = [](auto* /*ws*/) {
+            /* Check ws->getBufferedAmount() here */ },
+									 .ping = [](auto* /*ws*/, std::string_view) {
+            /* Not implemented yet */ },
+									 .pong = [](auto* /*ws*/, std::string_view) {
+            /* Not implemented yet */ },
+									 .close = [](auto* /*ws*/, int /*code*/, std::string_view /*message*/) {
+            /* You may access ws->getUserData() here */ } })
+		.listen(9001, [](auto* listen_socket) {
+			if(listen_socket) {
+				std::cout << "Listening on port " << 9001 << std::endl;
+			}
+		})
+		.run();
+
+	return 0;
 }
