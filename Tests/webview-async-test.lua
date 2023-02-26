93c78cbdb216b6ab95c1624040d07821562d7ffd
RDW <duckwhale@mail.ru>
2023-02-26 01:58:25 +0100

    Update webview-async-test.lua

--- Tests/webview-async-test.lua
+++ Tests/webview-async-test.lua
@@ -4,12 +4,17 @@ local webview_navigate = webview.bindings.webview_navigate
 local webview_run = webview.bindings.webview_run
 local webview_run_once = webview.bindings.webview_run_once
 local webview_terminate = webview.bindings.webview_terminate
+local webview_destroy = webview.bindings.webview_destroy
 
 -- TODO C_WebView.Create
 -- NavigateToURL
 -- RunForever
 -- RunOnce(inBlockingMode) -- OSX blocking, TBD?
 -- Shutdown/Close
+
+-- Docs:
+-- Scenario one: 1 webview, run blocking -> observation: async work pauses until window is close,d then resumes)
+-- 2: 1 webview, run_once blocking ->
 
 local view = webview_create(true, nil)
 
@@ -26,12 +31,16 @@ idle:start(function()
 	numEventLoopIerations = numEventLoopIerations + 1
 	print("Before I/O polling, no blocking", numEventLoopIerations)
 
-	-- BLOCKS the event loop (don't do this if you need other work to complete!)
-	webview_run(view)
-
+	-- webview_run_once(view, true)
 	if numEventLoopIerations == 100 then
 		print("Stopping event loop", numEventLoopIerations)
 		-- uv.stop()
 	end
 end)
+
+webview_run(view)
+-- BLOCKS the event loop (don't do this if you need other work to complete!)
+-- webview_terminate(view)
+webview_destroy(view)
+
 uv.run()
