b5bc25ed10db7ab044902cee1baeb1eb92695c82
RDW <duckwhale@mail.ru>
2023-02-26 06:47:26 +0100

    Update webview.cpp

--- webview.cpp
+++ webview.cpp
@@ -9,12 +9,12 @@ int main(int argc, char *argv[]) {
 
 	std::cout << "Hello WebView" << std::endl;
 
-  webview::webview w(true, nullptr);
-  webview::webview w2(true, nullptr);
-  w.set_title("Minimal example");
-  w.set_size(480, 320, WEBVIEW_HINT_NONE);
-  w.navigate("https://en.m.wikipedia.org/wiki/Main_Page");
-  w.run();
+	webview::webview w(true, nullptr);
+	webview::webview w2(true, nullptr);
+	w.set_title("Minimal example");
+	w.set_size(480, 320, WEBVIEW_HINT_NONE);
+	w.navigate("https://en.m.wikipedia.org/wiki/Main_Page");
+	w.run();
 
 	return 0;
 }
