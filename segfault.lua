4257d90873ef31b77a2944ec85ce00245141b602
RDW <duckwhale@mail.ru>
2023-02-26 06:11:32 +0100

    Update segfault.lua

--- segfault.lua
+++ segfault.lua
@@ -1,25 +1,21 @@
 local webview = require("webview")
-print("WTF")
+print(0)
 
 local view = webview.bindings.webview_create(true, nil)
-print("WTF")
+print(1)
+
+--webview.bindings.webview_run_once(view, false)
+				--webview.bindings.webview_terminate(view)
+print(2)
+
+--webview.bindings.webview_destroy(view)
+print(3)
+
+local view = webview.bindings.webview_create(true, nil)
+print(4)
 
 webview.bindings.webview_run_once(view, false)
 				--webview.bindings.webview_terminate(view)
-print("WTF")
-
+print(5)
 webview.bindings.webview_destroy(view)
-print("WTF")
-
-local view = webview.bindings.webview_create(true, nil)
-print("WTF")
-
-webview.bindings.webview_run_once(view, false)
-				--webview.bindings.webview_terminate(view)
-print("WTF")
-
-webview.bindings.webview_destroy(view)
-
-
---
-print("WTF")
+print(6)
