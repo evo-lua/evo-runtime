28baac4ed4511c4968666b360ba661fddc72cc93
RDW <duckwhale@mail.ru>
2023-02-26 03:42:13 +0100

    Update C_WebView.lua

--- Runtime/Bindings/C_WebView.lua
+++ Runtime/Bindings/C_WebView.lua
@@ -1,1 +1,3 @@
 -- https://github.com/webview/webview_deno/pull/112/files#diff-8374b0f8a86b17fbc3e686c8c47b2ab3eb7ede9bb463a3837987545782e30cbc
+
+-- https://github.com/eliassjogreen/webview/blob/34f4e39d56f21853311b93b438dcf8ece22dae73/webview.h
