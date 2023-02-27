8ab8bf0e5576296652c80804bfef4b1d8dc89886
RDW <duckwhale@mail.ru>
2023-02-27 06:26:10 +0100

    Update webview.cpp

--- webview.cpp
+++ webview.cpp
@@ -10,14 +10,14 @@ int main(int argc, char *argv[]) {
 	std::cout << "Hello WebView" << std::endl;
 
 	webview::webview w(true, nullptr);
-	webview::webview w2(true, nullptr);
+	// webview::webview w2(true, nullptr);
 	w.set_title("First window");
 	w.set_size(640, 480, WEBVIEW_HINT_NONE);
 	w.navigate("https://en.m.wikipedia.org/wiki/Main_Page");
 
-	w2.set_title("Second window");
-	w2.set_size(480, 320, WEBVIEW_HINT_NONE);
-	w2.navigate("https://github.com");
+	// w2.set_title("Second window");
+	// w2.set_size(480, 320, WEBVIEW_HINT_NONE);
+	// w2.navigate("https://github.com");
 
 	w.run();
 
