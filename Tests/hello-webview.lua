a8f4e7124865e0c9d1c4038c0a268dd1e6a4cf32
RDW <duckwhale@mail.ru>
2023-02-27 06:26:05 +0100

    Update hello-webview.lua

--- Tests/hello-webview.lua
+++ Tests/hello-webview.lua
@@ -1,4 +1,15 @@
 local webview = require("webview")
 
+-- webview::webview w(true, nullptr);
+-- // webview::webview w2(true, nullptr);
+-- w.set_title("First window");
+-- w.set_size(640, 480, WEBVIEW_HINT_NONE);
+-- w.navigate("https://en.m.wikipedia.org/wiki/Main_Page");
+
+local WEBVIEW_HINT_NONE = 0
 local wv = webview.bindings.webview_create(true, nil)
+webview.bindings.webview_set_size(wv, 640, 480, WEBVIEW_HINT_NONE);
+webview.bindings.webview_set_title(wv, "First window")
+webview.bindings.webview_navigate(wv, "https://en.m.wikipedia.org/wiki/Main_Page");
+
 webview.bindings.webview_run(wv)
