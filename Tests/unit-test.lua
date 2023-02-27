a50054290155ef340b744ac067edc37660ae2196
RDW <duckwhale@mail.ru>
2023-02-27 13:46:09 +0100

    Refactor: Streamline the entry point for unit tests

--- Tests/unit-test.lua
+++ Tests/unit-test.lua
@@ -1,9 +1,3 @@
-local assertions = require("assertions")
-local bdd = require("bdd")
-local transform = require("transform")
-
-local format = string.format
-
 -- All paths are relative to the project root, since that's where the CI run will start
 local specFiles = {
 	"Tests/BDD/globals.spec.lua",
@@ -12,41 +6,6 @@ local specFiles = {
 	"Tests/BDD/runtime-namespace.spec.lua",
 }
 
-local function runBasicTests()
-	bdd.setBasicReportMode()
-	local numFailedTests = bdd.startTestRunner(specFiles)
+local numFailedSections = C_Runtime.RunDetailedTests(specFiles)
 
-	print(bdd.getReport())
-
-	local errorDetails = bdd.getErrorDetails()
-	for index, errorInfo in ipairs(errorDetails) do
-		print(format("(Error #%d) in %s", index, transform.bold(errorInfo.specFile)))
-		print(transform.brightRed(errorInfo.stackTrace))
-		print()
-	end
-
-	return numFailedTests
-end
-
-local function runDetailedTests()
-	bdd.setDetailedReportMode()
-	local numFailedTests = bdd.startTestRunner(specFiles)
-
-	print(bdd.getReport())
-
-	local errorDetails = bdd.getErrorDetails()
-	for index, errorInfo in ipairs(errorDetails) do
-		print(format("(Error #%d) in %s", index, transform.bold(errorInfo.specFile)))
-		print(transform.brightRed(errorInfo.stackTrace))
-		print()
-	end
-
-	return numFailedTests
-end
-
-assertions.export() -- Should probably remove this after global aliases are set up by the runtime
-
-local numFailedSpecFiles = runBasicTests()
-local numFailedSections = runDetailedTests()
-
-os.exit(math.max(numFailedSpecFiles, numFailedSections))
+os.exit(numFailedSections)
