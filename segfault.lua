c25aeb058c47e53db5d58e506092d45d91cf88eb
RDW <duckwhale@mail.ru>
2023-02-27 05:56:26 +0100

    Update segfault.lua

--- segfault.lua
+++ segfault.lua
@@ -1,21 +1,10 @@
 local webview = require("webview")
-print(0)
 
 local view = webview.bindings.webview_create(true, nil)
-print(1)
-
---webview.bindings.webview_run_once(view, false)
-				--webview.bindings.webview_terminate(view)
-print(2)
-
---webview.bindings.webview_destroy(view)
-print(3)
+print("First WebView created")
 
 local view = webview.bindings.webview_create(true, nil)
-print(4)
+print("Second WebView created")
 
-webview.bindings.webview_run_once(view, false)
-				--webview.bindings.webview_terminate(view)
-print(5)
-webview.bindings.webview_destroy(view)
-print(6)
+local childview = webview.bindings.webview_create(true, webview.bindings.webview_get_window(view))
+print("Child created")
