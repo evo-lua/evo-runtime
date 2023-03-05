a9fb1e8a1c1b44183c7228c2382ee7c39863393b
RDW <duckwhale@mail.ru>
2023-03-05 19:29:48 +0100

    Tests: Add some tests for webview_run_once

--- Tests/BDD/webview-library.spec.lua
+++ Tests/BDD/webview-library.spec.lua
@@ -1,6 +1,9 @@
 local ffi = require("ffi")
-
+local uv = require("uv")
 local webview = require("webview")
+
+local webview_create = webview.bindings.webview_create
+local webview_run_once = webview.bindings.webview_run_once
 
 describe("webview", function()
 	describe("bindings", function()
@@ -28,6 +31,34 @@ describe("webview", function()
 			for _, functionName in ipairs(exportedApiSurface) do
 				assertEquals(type(webview.bindings[functionName]), "cdata")
 			end
+		end)
+
+		describe("run_once", function()
+			it("should not block the event loop", function()
+				local view = webview_create(true, nil)
+
+				local WEBVIEW_HINT_NONE = 0
+				webview.bindings.webview_set_size(view, 640, 480, WEBVIEW_HINT_NONE)
+				webview.bindings.webview_set_title(view, "Le window")
+
+				local numUpdates = 0
+
+				local TARGET_FPS = 60 -- Since timers are inherently at least a little inaccurate, might have to be increased ?
+				local GUI_UPDATE_INTERVAL_IN_MS = 1000 / TARGET_FPS
+				local guiUpdateTimer = uv.new_timer()
+
+				guiUpdateTimer:start(GUI_UPDATE_INTERVAL_IN_MS, GUI_UPDATE_INTERVAL_IN_MS, function()
+					numUpdates = numUpdates + 1
+					webview_run_once(view, false)
+
+					if numUpdates == 6 then
+						uv.stop()
+						webview.bindings.webview_destroy(view)
+					end
+				end)
+
+				uv.run()
+			end)
 		end)
 	end)
 
