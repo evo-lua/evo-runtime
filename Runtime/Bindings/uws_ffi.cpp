7278834f94e60fbb4a99f03288d2f1719388d150
RDW <duckwhale@mail.ru>
2023-03-06 17:28:02 +0100

    Update uws_ffi.cpp

--- Runtime/Bindings/uws_ffi.cpp
+++ Runtime/Bindings/uws_ffi.cpp
@@ -11,6 +11,42 @@ static void listen_handler(struct us_listen_socket_t* listen_socket, uws_app_lis
 	}
 }
 
+
+
+static void open_handler(uws_websocket_t* ws, void* user_data) {
+	std::cout << "open_handler" << std::endl;
+	/* Open event here, you may access uws_ws_get_user_data(WS) which points to a PerSocketData struct */
+}
+
+static void message_handler(uws_websocket_t* ws, const char* message, size_t length, uws_opcode_t opcode, void* user_data) {
+	std::cout << "message_handler" << std::endl;
+	uws_ws_send(SSL, ws, message, length, opcode);
+}
+
+static void close_handler(uws_websocket_t* ws, int code, const char* message, size_t length, void* user_data) {
+
+	std::cout << "close_handler" << std::endl;
+	/* You may access uws_ws_get_user_data(ws) here, but sending or
+	 * doing any kind of I/O with the socket is not valid. */
+}
+
+static void drain_handler(uws_websocket_t* ws, void* user_data) {
+	std::cout << "drain_handler" << std::endl;
+	/* Check uws_ws_get_buffered_amount(ws) here */
+}
+
+static void ping_handler(uws_websocket_t* ws, const char* message, size_t length, void* user_data) {
+	std::cout << "ping_handler" << std::endl;
+	/* You don't need to handle this one, we automatically respond to pings as per standard */
+}
+
+static void pong_handler(uws_websocket_t* ws, const char* message, size_t length, void* user_data) {
+	std::cout << "pong_handler" << std::endl;
+
+	/* You don't need to handle this one either */
+}
+
+
 struct static_uws_exports_table {
 	uws_listen_handler listen_handler;
 	uws_app_t* (*uws_create_app)(int ssl, struct us_socket_context_options_t options);
