c33ad0ddce86157214c4cdeb20cd1933846f062b
RDW <duckwhale@mail.ru>
2023-03-06 13:45:55 +0100

    Update uws-library.spec.lua

--- Tests/BDD/uws-library.spec.lua
+++ Tests/BDD/uws-library.spec.lua
@@ -9,23 +9,23 @@ describe("uws", function()
 	describe("bindings", function()
 		it("should export the entirety of the uws C API", function()
 			local exportedApiSurface = {
--- 				"webview_bind",
--- 				"webview_create",
--- 				"webview_destroy",
--- 				"webview_dispatch",
--- 				"webview_eval",
--- 				"webview_get_window",
--- 				"webview_init",
--- 				"webview_navigate",
--- 				"webview_return",
--- 				"webview_run",
--- 				"webview_run_once",
--- 				"webview_set_html",
--- 				"webview_set_size",
--- 				"webview_set_title",
--- 				"webview_terminate",
--- 				"webview_unbind",
--- 				"webview_version",
+				-- 				"webview_bind",
+				-- 				"webview_create",
+				-- 				"webview_destroy",
+				-- 				"webview_dispatch",
+				-- 				"webview_eval",
+				-- 				"webview_get_window",
+				-- 				"webview_init",
+				-- 				"webview_navigate",
+				-- 				"webview_return",
+				-- 				"webview_run",
+				-- 				"webview_run_once",
+				-- 				"webview_set_html",
+				-- 				"webview_set_size",
+				-- 				"webview_set_title",
+				-- 				"webview_terminate",
+				-- 				"webview_unbind",
+				-- 				"webview_version",
 			}
 
 			for _, functionName in ipairs(exportedApiSurface) do
@@ -33,51 +33,51 @@ describe("uws", function()
 			end
 		end)
 
--- 		describe("run_once", function()
--- 			it("should not block the event loop", function()
--- 				local view = webview_create(true, nil)
+		-- 		describe("run_once", function()
+		-- 			it("should not block the event loop", function()
+		-- 				local view = webview_create(true, nil)
 
--- 				local WEBVIEW_HINT_NONE = 0
--- 				webview.bindings.webview_set_size(view, 640, 480, WEBVIEW_HINT_NONE)
--- 				webview.bindings.webview_set_title(view, "Le window")
+		-- 				local WEBVIEW_HINT_NONE = 0
+		-- 				webview.bindings.webview_set_size(view, 640, 480, WEBVIEW_HINT_NONE)
+		-- 				webview.bindings.webview_set_title(view, "Le window")
 
--- 				local numUpdates = 0
+		-- 				local numUpdates = 0
 
--- 				local TARGET_FPS = 60 -- Since timers are inherently at least a little inaccurate, might have to be increased ?
--- 				local GUI_UPDATE_INTERVAL_IN_MS = 1000 / TARGET_FPS
--- 				local guiUpdateTimer = uv.new_timer()
+		-- 				local TARGET_FPS = 60 -- Since timers are inherently at least a little inaccurate, might have to be increased ?
+		-- 				local GUI_UPDATE_INTERVAL_IN_MS = 1000 / TARGET_FPS
+		-- 				local guiUpdateTimer = uv.new_timer()
 
--- 				guiUpdateTimer:start(GUI_UPDATE_INTERVAL_IN_MS, GUI_UPDATE_INTERVAL_IN_MS, function()
--- 					numUpdates = numUpdates + 1
--- 					webview_run_once(view, false)
+		-- 				guiUpdateTimer:start(GUI_UPDATE_INTERVAL_IN_MS, GUI_UPDATE_INTERVAL_IN_MS, function()
+		-- 					numUpdates = numUpdates + 1
+		-- 					webview_run_once(view, false)
 
--- 					if numUpdates == 6 then
--- 						uv.stop()
--- 						webview.bindings.webview_destroy(view)
--- 					end
--- 				end)
+		-- 					if numUpdates == 6 then
+		-- 						uv.stop()
+		-- 						webview.bindings.webview_destroy(view)
+		-- 					end
+		-- 				end)
 
--- 				uv.run()
--- 			end)
--- 		end)
--- 	end)
+		-- 				uv.run()
+		-- 			end)
+		-- 		end)
+		-- 	end)
 
--- 	-- This should be moved to the runtime library, once it is actually implemented...
--- 	describe("version", function()
--- 		it("should be a semantic version string", function()
--- 			local versionString = webview.version()
--- 			local major, minor, patch = string.match(versionString, "(%d+).(%d+).(%d+)")
+		-- 	-- This should be moved to the runtime library, once it is actually implemented...
+		-- 	describe("version", function()
+		-- 		it("should be a semantic version string", function()
+		-- 			local versionString = webview.version()
+		-- 			local major, minor, patch = string.match(versionString, "(%d+).(%d+).(%d+)")
 
--- 			assertEquals(type(major), "string")
--- 			assertEquals(type(minor), "string")
--- 			assertEquals(type(patch), "string")
--- 		end)
+		-- 			assertEquals(type(major), "string")
+		-- 			assertEquals(type(minor), "string")
+		-- 			assertEquals(type(patch), "string")
+		-- 		end)
 
--- 		it("should be the version number returned by webview_version()", function()
--- 			local versionInfo = webview.bindings.webview_version()
--- 			local expectedVersionString = ffi.string(versionInfo.version_number)
+		-- 		it("should be the version number returned by webview_version()", function()
+		-- 			local versionInfo = webview.bindings.webview_version()
+		-- 			local expectedVersionString = ffi.string(versionInfo.version_number)
 
--- 			assertEquals(webview.version(), expectedVersionString)
--- 		end)
+		-- 			assertEquals(webview.version(), expectedVersionString)
+		-- 		end)
 	end)
 end)
