7609864b1be89a21e33966407492d6ea25f4c1d9
RDW <duckwhale@mail.ru>
2023-03-20 20:57:25 +0100

    Tests: Add some unit test for the extend builtin

--- Tests/BDD/globals.spec.lua
+++ Tests/BDD/globals.spec.lua
@@ -49,4 +49,39 @@ describe("_G", function()
 			assertEquals(capturedOutput, "Hello printf\n")
 		end)
 	end)
+
+	describe("extend", function()
+		it("should still work if the prototype object doesn't have a metatable", function()
+			local child = {}
+			local parent = {}
+			function parent:hello() end
+
+			extend(child, parent)
+			assertEquals(child.hello, parent.hello)
+		end)
+
+		it("should set up a metatable such that the child inherits the prototype's functionality", function()
+			local child = {}
+			local parent = {}
+			function parent:thisFunctionShouldBeInherited() end
+
+			extend(child, parent)
+			assertEquals(child.thisFunctionShouldBeInherited, parent.thisFunctionShouldBeInherited)
+		end)
+
+		it("should copy all existing fields from the prototype's metatable", function()
+			local child = {}
+			local parent = {}
+			local grandparent = {}
+			function parent:thisFunctionShouldBeInherited() end
+			function grandparent:thisFunctionShouldAlsoBeInherited() end
+
+			extend(parent, grandparent)
+			extend(child, parent)
+
+			assertEquals(child.thisFunctionShouldBeInherited, parent.thisFunctionShouldBeInherited)
+			assertEquals(child.thisFunctionShouldAlsoBeInherited, grandparent.thisFunctionShouldAlsoBeInherited)
+			assertEquals(parent.thisFunctionShouldAlsoBeInherited, grandparent.thisFunctionShouldAlsoBeInherited)
+		end)
+	end)
 end)
