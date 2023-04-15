a3df7631350fe5aecc78aa6c7d09b07575e0e37e
RDW <duckwhale@mail.ru>
2023-04-16 01:25:10 +0200

    Runtime: Export the stduuid FFI APIs to stduuid_ffi.hpp
    This is required so that the uws C++ glue layer can generate UUIDs.

--- Runtime/Bindings/stduuid_ffi.hpp
+++ Runtime/Bindings/stduuid_ffi.hpp
@@ -10,6 +10,11 @@ struct static_stduuid_exports_table {
 	bool (*uuid_create_system)(uuid_rfc_string_t* result);
 };
 
+bool uuid_create_v4(uuid_rfc_string_t* result);
+bool uuid_create_mt19937(uuid_rfc_string_t* result);
+bool uuid_create_v5(const char* namespace_uuid_str, const char* name, uuid_rfc_string_t* result);
+bool uuid_create_system(uuid_rfc_string_t* result);
+
 namespace stduuid_ffi {
 	void* getExportsTable();
 }
