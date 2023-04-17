0f3c58ec1da925e09b149a585a0b401ca05bb9fd
RDW <duckwhale@mail.ru>
2023-04-17 17:22:25 +0200

    Repo: Add a cppcheck suppression list to allow some issues
    WebViews on Mac are nightmare fuel, so I elected to bury my head in the sand on this one until there's a better reason for revisiting the mess. Hopefully, that day will never come, or at least not very soon.

--- deps/cppcheck.sh
+++ deps/cppcheck.sh
@@ -5,7 +5,8 @@ cppcheck --version
 # These are unlikely to change, so hardcoding them should be fine...
 INCLUDE_DIRS="-I Runtime -I Runtime/Bindings"
 SRC_DIRS="Runtime/*"
+IGNORE_LIST=".cppcheck"
 
 RELEVANT_FILES=$(find $SRC_DIRS -type f \( -name '*.c' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \))
 
-cppcheck --enable=all --verbose --error-exitcode=127 $INCLUDE_DIRS $RELEVANT_FILES
+cppcheck --enable=all --error-exitcode=127 --suppressions-list=$IGNORE_LIST $INCLUDE_DIRS $RELEVANT_FILES
