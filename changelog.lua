4b26c4fcbd3a3aee5f9f4fd9a4b9175636820cb6
RDW <duckwhale@mail.ru>
2023-05-01 03:34:10 +0200

    Repo: Add a new changelog for the initial v0.0.1 release

---
+++ changelog.lua
@@ -0,0 +1,24 @@
+local changelog = {
+	["v0.0.1"] = {
+		-- Initial release (first C++ version)
+		newFeatures = {
+			"Simplistic command-line interface for running Lua scripts",
+			"Mostly-complete Lua port of the NodeJS path library",
+			"Builtin test runner (BDD style) and assertions library",
+			"Basic HTTP and WebSockets server APIs (using uWebSockets)",
+			"Embedded LuaJIT FFI bindings for webview, uws, and stduuid",
+			"Embedded Lua bindings for libuv, zlib, and openssl",
+			"Several extensions to the Lua standard libraries",
+			"High-level API namespace for file system access",
+			"Lua library for UUID generation (via stduuid)",
+		},
+		breakingChanges = {
+			"This version of the runtime is completely backwards-incompatible with the previous (C-based) iteration, though many APIs are still the same",
+		},
+	},
+	["v0.0.0"] = {
+		-- Initial commit (not actually releasable)
+	},
+}
+
+return changelog
