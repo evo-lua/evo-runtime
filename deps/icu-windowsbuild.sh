1dac0d9d696d4a98bd30e6f6ee6ae4d5f17815cb
RDW <duckwhale@mail.ru>
2023-05-03 18:56:24 +0200

    Update icu-windowsbuild.sh

--- deps/icu-windowsbuild.sh
+++ deps/icu-windowsbuild.sh
@@ -1,3 +1,6 @@
+# pacman -S --needed autoconf automake libtool make mingw-w64-x86_64-gcc
+# pacman -S autoconf-archive
+# TODO out-of-source build
 set -e
 
 NUM_PARALLEL_JOBS=$(nproc)
@@ -5,7 +8,6 @@ NUM_PARALLEL_JOBS=$(nproc)
 echo "Building target icu4c with $NUM_PARALLEL_JOBS jobs"
 
 SRC_DIR=deps/unicode-org/icu/icu4c/source
-BUILD_DIR=$SRC_DIR/cmakebuild-windows
 OUT_DIR=ninjabuild-windows
 
 cd $SRC_DIR
@@ -14,8 +16,16 @@ cd $SRC_DIR
 export CC=gcc
 export CXX=g++
 
-./runConfigureICU MinGW --enable-static --disable-shared
-make -j $NUM_PARALLEL_JOBS
+# Run autoreconf to generate the latest configure script
+autoreconf -i -f
+
+# ./runConfigureICU MinGW --enable-static --disable-shared  --disable-tools --disable-tests --disable-samples 
+./runConfigureICU MinGW --enable-static --enable-shared=no
+#--enable-static --enable-shared=no  --enable-tools=no --enable-tests=no --enable-samples=no
+# ./configure --enable-static --disable-shared  --disable-tools --disable-tests --disable-samples 
+#--prefix=$BUILD_DIR 
+make clean
+make -j$NUM_PARALLEL_JOBS
 
 cd -
 
