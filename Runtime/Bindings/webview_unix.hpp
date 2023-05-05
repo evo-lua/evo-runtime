4157fcaf6253d3650a0986456a4572f0f241b471
RDW <duckwhale@mail.ru>
2023-05-05 06:11:30 +0200

    Runtime: Implement webview_toggle_fullscreen for Linux
    It seems to have some issues with selecting the right monitor if there are multiple, but that's something to improve later.

--- Runtime/Bindings/webview_unix.hpp
+++ Runtime/Bindings/webview_unix.hpp
@@ -10,6 +10,14 @@ namespace webview_ffi {
 		int step(int blocking) {
 			return gtk_main_iteration_do(blocking);
 		}
-	};
 
+		void toggleFullScreen() {
+			GtkWindow* gtkWindow = GTK_WINDOW(window());
+			GdkWindow* gdkWindow = gtk_widget_get_window(GTK_WIDGET(gtkWindow));
+			GdkWindowState state = gdk_window_get_state(gdkWindow);
+
+			if(state & GDK_WINDOW_STATE_FULLSCREEN) gtk_window_unfullscreen(gtkWindow);
+			else gtk_window_fullscreen(gtkWindow);
+		}
+	};
 }
