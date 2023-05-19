5ec76a4aa298bf1025c012c6936f9f8fc6d1a91c
RDW <duckwhale@mail.ru>
2023-05-19 10:46:24 +0200

    Runtime: Add aliases for parse and stringify to the json library

--- Runtime/Extensions/jsonx.lua
+++ Runtime/Extensions/jsonx.lua
@@ -1,7 +1,17 @@
 local json = require("json")
 
+local json_decode = json.decode
+local json_encode = json.encode
 local string_match = string.match
 
 function json.version()
 	return string_match(json._VERSION, "v(%d+.%d+.%d+)") -- Strip the leading v
+end
+
+function json.parse(jsonString)
+	return json_decode(jsonString)
+end
+
+function json.stringify(luaTable)
+	return json_encode(luaTable)
 end
