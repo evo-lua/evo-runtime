e058944874797bc3db3a0fe3e580bb1b96d595b1
RDW <duckwhale@mail.ru>
2023-05-19 04:06:18 +0200

    Repo: Add build scripts for lua-rapidjson

--- deps/unixbuild-all.sh
+++ deps/unixbuild-all.sh
@@ -3,6 +3,7 @@ set -e
 deps/luajit-unixbuild.sh
 deps/openssl-unixbuild.sh
 deps/luaopenssl-unixbuild.sh
+deps/rapidjson-unixbuild.sh
 deps/luv-unixbuild.sh
 deps/uws-unixbuild.sh
 deps/zlib-unixbuild.sh
--- deps/windowsbuild-all.sh
+++ deps/windowsbuild-all.sh
@@ -4,6 +4,7 @@ deps/fetch-mswebview2.sh
 deps/luajit-windowsbuild.sh
 deps/openssl-windowsbuild.sh
 deps/luaopenssl-windowsbuild.sh
+deps/rapidjson-windowsbuild.sh
 deps/luv-windowsbuild.sh
 deps/uws-windowsbuild.sh
 deps/zlib-windowsbuild.sh
