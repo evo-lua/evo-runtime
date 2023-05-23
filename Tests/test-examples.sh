4f9ce607cd124d14f75bc33398bc007e61e58a64
RDW <duckwhale@mail.ru>
2023-05-24 01:34:57 +0200

    Create test-examples.sh

---
+++ Tests/test-examples.sh
@@ -0,0 +1,26 @@
+set -e
+
+EXAMPLES_DIR="Tests/Snapshots"
+LUA_RUNTIME="evo"
+
+for EXAMPLE_SCRIPT in $EXAMPLES_DIR/*.lua
+do
+    BASE_NAME=$(basename $EXAMPLE_SCRIPT .lua)
+    OUTPUT_FILE=$EXAMPLES_DIR/$BASE_NAME.out.txt
+    EXPECTED_FILE=$EXAMPLES_DIR/$BASE_NAME.expected.txt
+    
+    echo "Running snapshot test for example $BASE_NAME..."
+    
+    $LUA_RUNTIME $EXAMPLE_SCRIPT > $OUTPUT_FILE
+
+    # diff $OUTPUT_FILE $EXPECTED_FILE
+    if diff -q $OUTPUT_FILE $EXPECTED_FILE >/dev/null
+    then
+        echo "$BASE_NAME: PASSED"
+    else
+        echo "$BASE_NAME: FAILED"
+        diff $OUTPUT_FILE $EXPECTED_FILE
+        exit 1
+    fi
+	rm $OUTPUT_FILE
+done
