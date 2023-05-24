4f7ec8e8aee57ecafd41049b9e73fe3dad78c10a
RDW <duckwhale@mail.ru>
2023-05-24 02:03:57 +0200

    Update test-examples.sh

--- Tests/test-examples.sh
+++ Tests/test-examples.sh
@@ -1,29 +1,27 @@
-set -e
-
 EXAMPLES_DIR="Tests/Snapshots"
 LUA_RUNTIME="evo"
+CLI_ARGS="cli args go here"
 EXIT_CODE=0
 
 for EXAMPLE_SCRIPT in $EXAMPLES_DIR/*.lua
 do
     BASE_NAME=$(basename $EXAMPLE_SCRIPT .lua)
-    OUTPUT_FILE=$EXAMPLES_DIR/$BASE_NAME.out.txt
+    OUTPUT_FILE=$EXAMPLES_DIR/$BASE_NAME.actual.txt
     EXPECTED_FILE=$EXAMPLES_DIR/$BASE_NAME.expected.txt
     
     echo "Running snapshot test for example $BASE_NAME..."
     
-    $LUA_RUNTIME $EXAMPLE_SCRIPT > $OUTPUT_FILE
+    $LUA_RUNTIME $EXAMPLE_SCRIPT $CLI_ARGS > $OUTPUT_FILE
 
-    # diff $OUTPUT_FILE $EXPECTED_FILE
     if diff -q $OUTPUT_FILE $EXPECTED_FILE >/dev/null
     then
-        echo "$BASE_NAME: PASSED"
+        echo "PASS	$BASE_NAME"
     else
-        echo "$BASE_NAME: FAILED"
+        echo "FAIL	$BASE_NAME"
         diff $OUTPUT_FILE $EXPECTED_FILE
         EXIT_CODE=1
     fi
-	rm $OUTPUT_FILE
 done
 
+rm $OUTPUT_FILE
 exit $EXIT_CODE
