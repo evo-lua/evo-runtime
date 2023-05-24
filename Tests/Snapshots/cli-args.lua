91a64e6878b15f5ecc0be0cf1fec23a22e172a32
RDW <duckwhale@mail.ru>
2023-05-24 02:03:44 +0200

    Update cli-args.lua

--- Tests/Snapshots/cli-args.lua
+++ Tests/Snapshots/cli-args.lua
@@ -1,8 +1,5 @@
 print("Number of command-line arguments received:", #arg)
 
-print("Dumping command-line arguments...")
-dump(arg)
-
 print("Iterating over command-line arguments...")
 for index, argument in ipairs(arg) do
 	print(index, argument)
