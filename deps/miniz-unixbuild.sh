dbe94d65f84b1eebf4286293d11d833f17300e42
RDW <duckwhale@mail.ru>
2023-06-13 22:38:22 +0200

    Repo: Add build scripts for amalgamating miniz

---
+++ deps/miniz-unixbuild.sh
@@ -0,0 +1,13 @@
+set -e
+
+echo "Building target miniz"
+
+OUT_DIR=ninjabuild-unix
+SRC_DIR=deps/richgel999/miniz
+BUILD_DIR=$SRC_DIR/cmakebuild-unix
+
+cmake -S $SRC_DIR -B $BUILD_DIR -G Ninja -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_COMPILER=gcc -DBUILD_EXAMPLES=OFF -DINSTALL_PROJECT=OFF -DAMALGAMATE_SOURCES=ON
+cmake --build $BUILD_DIR --clean-first
+
+cp $BUILD_DIR/libminiz.a $OUT_DIR
+cp $BUILD_DIR/amalgamation/miniz.h $OUT_DIR
