50be780e1fff763a29cf08f33bf27cdfec6298d5
RDW <duckwhale@mail.ru>
2023-06-15 12:24:40 +0200

    Runtime: Add a new RunSnapshotTests method to the API

--- Runtime/API/C_Runtime.lua
+++ Runtime/API/C_Runtime.lua
@@ -75,4 +75,29 @@ function C_Runtime.PrintVersionString()
 	print(EVO_VERSION)
 end
 
+local function shell_exec(shellCommand)
+	local file = assert(io.popen(shellCommand, "r"))
+
+	file:flush() -- Required to prevent receiving only partial output
+	local output = file:read("*all")
+	file:close()
+
+	return output
+end
+
+function C_Runtime.RunSnapshotTests(testCases)
+	validation.validateTable(testCases, "testCases")
+
+	for descriptor, testInfo in pairs(testCases) do
+		validation.validateString(testInfo.programToRun, "programToRun")
+		validation.validateFunction(testInfo.onExit, "onExit")
+
+		printf("Running snapshot test %s", transform.bold(descriptor))
+		printf("[SHELL] Executing command: %s", transform.green(testInfo.programToRun))
+		local observedOutput = shell_exec(testInfo.programToRun)
+		testInfo.onExit(observedOutput)
+		printf("[SHELL] Observed output has %d bytes", #observedOutput)
+	end
+end
+
 return C_Runtime
