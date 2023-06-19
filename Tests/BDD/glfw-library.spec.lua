cff9608adf6b866bd8c82b7d61d36a5e74e05727
RDW <duckwhale@mail.ru>
2023-06-19 15:23:05 +0200

    Tests: Add some unit tests for the glfw library

---
+++ Tests/BDD/glfw-library.spec.lua
@@ -0,0 +1,39 @@
+local glfw = require("glfw")
+
+describe("glfw", function()
+	describe("bindings", function()
+		it("should export the entirety of the glfw API", function()
+			local exportedApiSurface = {
+				"glfw_version",
+				"glfw_find_constant",
+			}
+
+			for _, functionName in ipairs(exportedApiSurface) do
+				assertEquals(type(glfw.bindings[functionName]), "cdata")
+			end
+		end)
+	end)
+
+	describe("glfw_find_constant", function()
+		it("should return the defined value if the constant exists", function()
+			local value = glfw.bindings.glfw_find_constant("GLFW_KEY_0")
+			assertEquals(value, 48)
+		end)
+
+		it("should return a special sentinel value if the constant does not exist", function()
+			local value = glfw.bindings.glfw_find_constant("DOES_NOT_EXIST")
+			assertEquals(value, 0xdead)
+		end)
+	end)
+
+	describe("version", function()
+		it("should return the embedded glfw version in semver format", function()
+			local versionString = glfw.version()
+			local firstMatchedCharacterIndex, lastMatchedCharacterIndex = string.find(versionString, "%d+.%d+.%d+")
+
+			assertEquals(firstMatchedCharacterIndex, 1)
+			assertEquals(lastMatchedCharacterIndex, string.len(versionString))
+			assertEquals(type(string.match(versionString, "%d+.%d+.%d+")), "string")
+		end)
+	end)
+end)
