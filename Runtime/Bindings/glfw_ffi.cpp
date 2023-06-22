ac505be5b5d54e5a842413d13ddef523f6026535
RDW <duckwhale@mail.ru>
2023-06-22 21:40:03 +0200

    Runtime: Add callbacks for GLFW windowing events

--- Runtime/Bindings/glfw_ffi.cpp
+++ Runtime/Bindings/glfw_ffi.cpp
@@ -24,12 +24,116 @@ void glfw_register_events(GLFWwindow* window, std::queue<deferred_event_t>* queu
 	glfwSetWindowUserPointer(window, queue);
 
 	glfw_set_window_move_callback(window, queue);
+	glfw_set_window_resize_callback(window, queue);
+	glfw_set_window_close_callback(window, queue);
+	glfw_set_framebuffer_resize_callback(window, queue);
+	glfw_set_content_scale_callback(window, queue);
+	glfw_set_window_refresh_callback(window, queue);
+	glfw_set_window_focus_callback(window, queue);
+	glfw_set_window_iconify_callback(window, queue);
+	glfw_set_window_maximize_callback(window, queue);
 }
 
 void glfw_set_window_move_callback(GLFWwindow* window, std::queue<deferred_event_t>* queue) {
 	glfwSetWindowPosCallback(window, [](GLFWwindow* window, int screenX, int screenY) {
 		window_move_event_t payload { .type = WINDOW_MOVE_EVENT, .x = screenX, .y = screenY };
 		deferred_event_t event { .window_move_details = payload };
+
+		auto userdata = glfwGetWindowUserPointer(window);
+		auto queue = static_cast<std::queue<deferred_event_t>*>(userdata);
+
+		queue->push(event);
+	});
+}
+
+void glfw_set_window_resize_callback(GLFWwindow* window, std::queue<deferred_event_t>* queue) {
+	glfwSetWindowSizeCallback(window, [](GLFWwindow* window, int width, int height) {
+		window_resize_event_t payload { .type = WINDOW_RESIZE_EVENT, .width = width, .height = height };
+		deferred_event_t event { .window_resize_details = payload };
+
+		auto userdata = glfwGetWindowUserPointer(window);
+		auto queue = static_cast<std::queue<deferred_event_t>*>(userdata);
+
+		queue->push(event);
+	});
+}
+
+void glfw_set_window_close_callback(GLFWwindow* window, std::queue<deferred_event_t>* queue) {
+	glfwSetWindowCloseCallback(window, [](GLFWwindow* window) {
+		window_close_event_t payload { .type = WINDOW_CLOSE_EVENT };
+		deferred_event_t event { .window_close_details = payload };
+
+		auto userdata = glfwGetWindowUserPointer(window);
+		auto queue = static_cast<std::queue<deferred_event_t>*>(userdata);
+
+		queue->push(event);
+	});
+}
+
+void glfw_set_framebuffer_resize_callback(GLFWwindow* window, std::queue<deferred_event_t>* queue) {
+	glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height) {
+		framebuffer_resize_event_t payload { .type = FRAMEBUFFER_RESIZE_EVENT, .width = width, .height = height };
+		deferred_event_t event { .framebuffer_resize_details = payload };
+
+		auto userdata = glfwGetWindowUserPointer(window);
+		auto queue = static_cast<std::queue<deferred_event_t>*>(userdata);
+
+		queue->push(event);
+	});
+}
+
+void glfw_set_content_scale_callback(GLFWwindow* window, std::queue<deferred_event_t>* queue) {
+	glfwSetWindowContentScaleCallback(window, [](GLFWwindow* window, float xScale, float yScale) {
+		content_scale_event_t payload { .type = CONTENT_SCALE_EVENT, .x = xScale, .y = yScale };
+		deferred_event_t event { .content_scale_details = payload };
+
+		auto userdata = glfwGetWindowUserPointer(window);
+		auto queue = static_cast<std::queue<deferred_event_t>*>(userdata);
+
+		queue->push(event);
+	});
+}
+
+void glfw_set_window_refresh_callback(GLFWwindow* window, std::queue<deferred_event_t>* queue) {
+	glfwSetWindowRefreshCallback(window, [](GLFWwindow* window) {
+		window_refresh_event_t payload { .type = WINDOW_REFRESH_EVENT };
+		deferred_event_t event { .window_refresh_details = payload };
+
+		auto userdata = glfwGetWindowUserPointer(window);
+		auto queue = static_cast<std::queue<deferred_event_t>*>(userdata);
+
+		queue->push(event);
+	});
+}
+
+void glfw_set_window_focus_callback(GLFWwindow* window, std::queue<deferred_event_t>* queue) {
+	glfwSetWindowFocusCallback(window, [](GLFWwindow* window, int focused) {
+		window_focus_event_t payload { .type = WINDOW_FOCUS_EVENT, .focused = focused };
+		deferred_event_t event { .window_focus_details = payload };
+
+		auto userdata = glfwGetWindowUserPointer(window);
+		auto queue = static_cast<std::queue<deferred_event_t>*>(userdata);
+
+		queue->push(event);
+	});
+}
+
+void glfw_set_window_iconify_callback(GLFWwindow* window, std::queue<deferred_event_t>* queue) {
+	glfwSetWindowIconifyCallback(window, [](GLFWwindow* window, int iconified) {
+		window_iconify_event_t payload { .type = WINDOW_ICONIFY_EVENT, .iconified = iconified };
+		deferred_event_t event { .window_iconify_details = payload };
+
+		auto userdata = glfwGetWindowUserPointer(window);
+		auto queue = static_cast<std::queue<deferred_event_t>*>(userdata);
+
+		queue->push(event);
+	});
+}
+
+void glfw_set_window_maximize_callback(GLFWwindow* window, std::queue<deferred_event_t>* queue) {
+	glfwSetWindowMaximizeCallback(window, [](GLFWwindow* window, int maximized) {
+		window_maximize_event_t payload { .type = WINDOW_MAXIMIZE_EVENT, .maximized = maximized };
+		deferred_event_t event { .window_maximize_details = payload };
 
 		auto userdata = glfwGetWindowUserPointer(window);
 		auto queue = static_cast<std::queue<deferred_event_t>*>(userdata);
