9f9bceba8d47646ed61aa7c10345042abe2e0cc1
RDW <duckwhale@mail.ru>
2023-09-28 11:39:55 +0200

    Perf: Create benchmarks for the Base64 bindings

---
+++ Benchmarks/base64-encode.lua
@@ -0,0 +1,70 @@
+local console = require("console")
+local crypto = require("crypto")
+local openssl = require("openssl")
+
+local tinsert = table.insert
+
+local SAMPLE_SIZE = 10000
+
+printf("Generating %d randomized samples of varying lengths", SAMPLE_SIZE)
+console.startTimer("Generate random samples")
+
+local inputs = {}
+
+for i = 1, SAMPLE_SIZE, 1 do
+	tinsert(inputs, openssl.random(4))
+	tinsert(inputs, openssl.random(8))
+	tinsert(inputs, openssl.random(16))
+	tinsert(inputs, openssl.random(32))
+	tinsert(inputs, openssl.random(64))
+	tinsert(inputs, openssl.random(128))
+	tinsert(inputs, openssl.random(256))
+	tinsert(inputs, openssl.random(512))
+	tinsert(inputs, openssl.random(1250))
+	tinsert(inputs, openssl.random(2500))
+	tinsert(inputs, openssl.random(5000))
+	tinsert(inputs, openssl.random(10000))
+	tinsert(inputs, openssl.random(100000))
+end
+
+console.stopTimer("Generate random samples")
+
+math.randomseed(os.clock())
+local availableBenchmarks = {
+	function()
+		console.startTimer("[Base64] Encoding with LuaOpenSSL")
+		for i = 1, SAMPLE_SIZE, 1 do
+			openssl.base64(inputs[i])
+		end
+		console.stopTimer("[Base64] Encoding with LuaOpenSSL")
+	end,
+
+	function()
+		console.startTimer("[Base64] Encoding with FFI bindings to OpenSSL")
+		for i = 1, SAMPLE_SIZE, 1 do
+			crypto.toBase64(inputs[i])
+		end
+		console.stopTimer("[Base64] Encoding with FFI bindings to OpenSSL")
+	end,
+
+	function()
+		console.startTimer("[Base64] Encoding with FFI bindings to Argon2")
+		for i = 1, SAMPLE_SIZE, 1 do
+			crypto.toCompactBase64(inputs[i])
+		end
+		console.stopTimer("[Base64] Encoding with FFI bindings to Argon2")
+	end,
+}
+
+local function shuffle(tbl)
+	for i = #tbl, 2, -1 do
+		local j = math.random(i)
+		tbl[i], tbl[j] = tbl[j], tbl[i]
+	end
+end
+
+shuffle(availableBenchmarks)
+
+for _, benchmark in ipairs(availableBenchmarks) do
+	benchmark()
+end
