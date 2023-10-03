56cf11b357ea3d8be25632443b4b28c91e53e312
RDW <duckwhale@mail.ru>
2023-10-03 13:14:27 +0200

    Tests: Add a visual test for console.clear
    No idea how to test this in an automated fashion...

---
+++ Tests/Visual/test-console-clear.lua
@@ -0,0 +1,5 @@
+local console = require("console")
+
+print("You should never see this text")
+console.clear()
+print("This is the only text that should be visible in the terminal (as the very first line)")
