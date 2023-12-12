c04b9b70a06d9ab75640b396d0548e699063ae45
RDW <duckwhale@mail.ru>
2023-12-12 02:00:01 +0100

    Runtime: Fix a crash in os.exit if close is set to true
    Need to manually unassign the uws loop here as otherwise there'll be a SEGFAULT when LuaJIT shuts down the Lua state and luv calls the active handles (which should have been removed by this time).

--- Runtime/main.cpp
+++ Runtime/main.cpp
@@ -52,6 +52,7 @@ int main(int argc, char* argv[]) {
 	// A bit of a hack; Can't use uv_default_loop because luv maintains a separate "default" loop of its own
 	uv_loop_t* loop = luv_loop(luaVM->GetState());
 	auto uwsEventLoop = uws_ffi::assignEventLoop(loop);
+	luaVM->AssignGlobalVariable("UWS_EVENT_LOOP", static_cast<void*>(uwsEventLoop));
 
 	std::string mainChunk = "local evo = require('evo'); return evo.run()";
 	std::string chunkName = "=(Lua entry point, at " FROM_HERE ")";
