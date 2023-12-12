da4de5a5299711c8bb3fd4a71da0c993676eacb3
RDW <duckwhale@mail.ru>
2023-12-12 05:43:37 +0100

    Runtime: Improve error handling in the iconv bindings

--- Runtime/Bindings/iconv_ffi.hpp
+++ Runtime/Bindings/iconv_ffi.hpp
@@ -1,9 +1,16 @@
 #pragma once
 
 #include <cstddef>
+#include <cstdint>
+
+typedef struct iconv_result_t {
+	uint8_t status_code;
+	size_t num_bytes_written;
+	const char* message;
+} iconv_result_t;
 
 struct static_iconv_exports_table {
-	size_t (*iconv_convert)(char* input, size_t input_size, const char* input_encoding, const char* output_encoding, char* output, size_t output_size);
+	iconv_result_t (*iconv_convert)(char* input, size_t input_size, const char* input_encoding, const char* output_encoding, char* output, size_t output_size);
 };
 
 namespace iconv_ffi {
