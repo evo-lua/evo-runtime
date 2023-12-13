909c51e8028d68bd149a1396e22b834175243ac5
RDW <duckwhale@mail.ru>
2023-12-13 21:21:24 +0100

    Update oop.lua

--- Runtime/Libraries/oop.lua
+++ Runtime/Libraries/oop.lua
@@ -1,6 +1,8 @@
 local middleclass = require("middleclass")
 
-local oop = {}
+local oop = {
+	registeredClasses = {},
+}
 
 function oop.version()
 	return string.match(middleclass._VERSION, "middleclass v(.+)")
@@ -8,16 +10,29 @@ end
 
 function oop.class(className)
 	-- TODO validate string
+	if oop.registeredClasses[className] then
+		error(format("Failed to register class %s (a class with this name already exists)", className), 0)
+	end
+
 	local class = middleclass.class(className)
+	oop.registeredClasses[className] = class
 	-- local mt = getmetatable(class)
 	-- mt.__call = function(self, ...)
 	-- 	return self:Construct(...)
 	-- end
+	-- function class:initialize(...)
+	-- 	return self:Construct(...)
+	-- end
+
 	return class
 end
 
 function oop.new(class)
-	return class.new(class)
+	local instance = class.new(class)
+	function instance:initialize(...)
+		return instance:Construct(...)
+	end
+	return instance
 end
 
 return oop
