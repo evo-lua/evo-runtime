94160a398817b8e7c33df3d7c25cf43f6816e75d
RDW <duckwhale@mail.ru>
2023-12-13 21:36:05 +0100

    Update oop-library.spec.lua

--- Tests/BDD/oop-library.spec.lua
+++ Tests/BDD/oop-library.spec.lua
@@ -25,7 +25,7 @@ describe("oop", function()
 			someObject:DoSomething()
 			assertTrue(didCallInheritedMethod)
 		end)
-		
+
 		it("should store the given class name in the class object", function()
 			local TestClass2 = oop.class("TestClass2")
 
@@ -51,14 +51,14 @@ describe("oop", function()
 		it("should allow each created instance to access class variables", function()
 			local TestClass4 = oop.class("TestClass4")
 			TestClass4.SOME_CONSTANT_VALUE = 1234
-			local 			instance = oop.new(TestClass4)
+			local instance = oop.new(TestClass4)
 
 			assertEquals(instance.SOME_CONSTANT_VALUE, 1234)
 		end)
 
 		it("should allow each created instance to access instance variables independently", function()
 			local TestClass5 = oop.class("TestClass5")
-			
+
 			local instanceA = oop.new(TestClass5)
 			instanceA.foo = 12345
 			local instanceB = oop.new(TestClass5)
@@ -81,30 +81,56 @@ describe("oop", function()
 
 			instance = oop.new(TestClass6)
 
+			-- assertEquals(FerociousKillerRabbit.foo, 42)
+			assertEquals(instance.foo, 42)
+			-- assertEquals(instance.foo, 44)
+		end)
+
+		it("should invoke the standard constructor if one was created", function()
+			local TestClass7 = oop.class("TestClass7")
+
+			function TestClass7:initialize()
+				self.foo = 42
+			end
+
+			function TestClass7:Construct()
+				self.foo = 44
+			end
+
+			local instance = oop.new(TestClass7)
+			-- instance = TestClass7()
+
+			-- assertEquals(FerociousKillerRabbit.foo, 42)
+			-- assertEquals(instance.foo, 42)
+			assertEquals(instance.foo, 44)
+		end)
+
+		it("should invoke the fallback constructor if none was created", function()
+			local TestClass8 = oop.class("TestClass8")
+
+			function TestClass8:initialize()
+				self.foo = 42
+			end
+
+			-- function TestClass7:Construct()
+			-- 	self.foo = 44
+			-- end
+
+			local instance = oop.new(TestClass8)
+			-- instance = TestClass7()
 
 			-- assertEquals(FerociousKillerRabbit.foo, 42)
 			assertEquals(instance.foo, 42)
 			-- assertEquals(instance.foo, 44)
 		end)
 
-		it("should invoke the constructor if one was created", function()
-			local TestClass7 = oop.class("TestClass7")
-
-			-- function FerociousKillerRabbit:initialize()
-			-- 	self.foo = 42
-			-- end
-
-			function TestClass7:Construct()
-				self.foo = 44
-			end
-
-			-- instance = oop.new(FerociousKillerRabbit)
-			instance = TestClass7()
-
-
-			-- assertEquals(FerociousKillerRabbit.foo, 42)
-			-- assertEquals(instance.foo, 42)
-			assertEquals(instance.foo, 44)
-		end)
+		-- it("should set up the instance metatable so that accessing nonexistent fields throws", function()
+		-- 	local TestClass8 = oop.class("TestClass8")
+		-- 	local instance = TestClass8()
+		-- 	assertThrows(function()
+		-- 		instance.foo = 42
+		-- 		local foo = instance.bar
+		-- 	end, "???")
+		-- end)
 	end)
 end)
