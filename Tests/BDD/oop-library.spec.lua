10a2e002c19106cafec121373bd6e7f94a563d67
RDW <duckwhale@mail.ru>
2023-12-13 19:51:09 +0100

    Update oop-library.spec.lua

--- Tests/BDD/oop-library.spec.lua
+++ Tests/BDD/oop-library.spec.lua
@@ -2,8 +2,13 @@ local oop = require("oop")
 
 describe("oop", function()
 	describe("version", function()
-		it("should be equivalent to the embedded middleclass _VERSION tag", function()
-		
+		it("hould return a semantic version string", function()
+			local versionString = oop.version()
+			local major, minor, patch = string.match(versionString, "(%d+).(%d+).(%d+)")
+
+			assertEquals(type(major), "string")
+			assertEquals(type(minor), "string")
+			assertEquals(type(patch), "string")
 		end)
 	end)
 end)
