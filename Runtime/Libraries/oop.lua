8e9c07bfc70b7687facdff7d51b316b5ff4cde00
RDW <duckwhale@mail.ru>
2023-12-13 19:58:35 +0100

    Update oop.lua

--- Runtime/Libraries/oop.lua
+++ Runtime/Libraries/oop.lua
@@ -2,8 +2,17 @@ local middleclass = require("middleclass")
 
 local oop = {}
 
-function oop:version()
+function oop.version()
 	return string.match(middleclass._VERSION, "middleclass v(.+)")
+end
+
+function oop.class(className)
+	-- TODO validate string
+	return middleclass.class(className)
+end
+
+function oop.new(class)
+	return class.new(class)
 end
 
 return oop
