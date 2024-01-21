09697bae8380ffea9ef11579ef33fd9abdcaafe1
RDW <duckwhale@mail.ru>
2024-01-21 04:56:38 +0100

    Build: Enable n2 support for speeding up local rebuilds
    I don't care about Windows support for n2 since there's no reason to do development there for most situations; it's just too slow and n2 won't help much (but it could be added later - I simply don't care to do the work required right now).

--- unixbuild.sh
+++ unixbuild.sh
@@ -15,7 +15,15 @@ $LUAJIT_EXE ninjabuild.lua
 # LuaJIT's jit module is implemented in Lua and needs to be loaded via LUA_PATH for bytecode generation
 export LUA_PATH="$BUILD_DIR/?.lua;./?.lua"
 
+if which n2 > /dev/null; then # Using n2 speeds up local development as it has better change detection
+    BUILD_TOOL=n2
+else # Ninja works just as well and is expected to be installed in all cases (required for building deps via CMake)
+    BUILD_TOOL=ninja
+fi
+
+echo "Selected build tool: $BUILD_TOOL"
+
 # This will only work after the dependencies have been built! (Run the deps/build-X.sh scripts manually at least once)
 # The reason this is excluded from the ninja build is to eliminate propagated errors that are difficult to debug/misleading
 # It's much easier to see if the dependencies could be built independently and they don't usually need rebuilding anyway
-ninja
+$BUILD_TOOL
