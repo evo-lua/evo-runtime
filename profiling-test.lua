b1771f1f377f0e03d8b1c1959485190cee8ff6a0
RDW <duckwhale@mail.ru>
2024-02-04 10:06:18 +0100

    Update profiling-test.lua

--- profiling-test.lua
+++ profiling-test.lua
@@ -1,6 +1,8 @@
+-- local profiler = require("jit.p")
 local profiler = require("profiler")
 
-profiler.start("G", "results.txt")
+-- profiler.start("G", "results.txt")
+profiler.start()
 for i=1, 56 + 1, 1 do
 	local license = C_FileSystem.ReadFile("LICENSE")
 	print(license)
