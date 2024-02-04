1fb10556c4d041edbe0611ed1c35e3938b29ea43
RDW <duckwhale@mail.ru>
2024-02-04 08:40:52 +0100

    Update profiling-test.lua

--- profiling-test.lua
+++ profiling-test.lua
@@ -2,7 +2,7 @@ local profiler = require("profiler")
 
 profiler.start("G", "results.txt")
 for i=1, 56 + 1, 1 do
-	-- print("Hello world")
+	print("Hello world")
 end
 profiler.stop()
 -- profiler.dumpstack("f")
