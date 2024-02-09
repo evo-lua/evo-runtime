278150ff459880a58bbd82e2dbf2711c90601a22
RDW <duckwhale@mail.ru>
2024-02-09 05:41:49 +0100

    Cleanup: Remove the redundant LuaJIT main function
    This isn't needed for the REPL, as evo has its own entry point.

--- Runtime/luajit_repl.c
+++ Runtime/luajit_repl.c
@@ -549,21 +549,3 @@ static int pmain(lua_State* L) {
 	}
 	return 0;
 }
-
-int main(int argc, char** argv) {
-	int status;
-	lua_State* L;
-	if(!argv[0]) argv = empty_argv;
-	else if(argv[0][0]) progname = argv[0];
-	L = lua_open();
-	if(L == NULL) {
-		l_message("cannot create state: not enough memory");
-		return EXIT_FAILURE;
-	}
-	smain.argc = argc;
-	smain.argv = argv;
-	status = lua_cpcall(L, pmain, NULL);
-	report(L, status);
-	lua_close(L);
-	return (status || smain.status > 0) ? EXIT_FAILURE : EXIT_SUCCESS;
-}
