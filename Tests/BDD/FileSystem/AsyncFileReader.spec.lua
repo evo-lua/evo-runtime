ecdfc3d5cf416d12fe07a8b72a45acf2ea7e734b
RDW <duckwhale@mail.ru>
2024-02-22 05:58:38 +0100

    Update AsyncFileReader.spec.lua

--- Tests/BDD/FileSystem/AsyncFileReader.spec.lua
+++ Tests/BDD/FileSystem/AsyncFileReader.spec.lua
@@ -3,7 +3,7 @@ local uv = require("uv")
 
 local AsyncFileReader = require("Runtime.API.FileSystem.AsyncFileReader") -- C_FileSystem.AsyncFileReader
 
-local function assertEventTrigger(functionToObserve, expectedEvent, expectedPayload, numExpectedNotifications)
+local function assertEvent(functionToObserve, expectedEvent, expectedPayload, numExpectedNotifications)
 	numExpectedNotifications = numExpectedNotifications or 1
 
 	etrace.clear()
@@ -25,27 +25,23 @@ local function assertEventTrigger(functionToObserve, expectedEvent, expectedPayl
 	if wasEventEnabled then
 		etrace.enable(expectedEvent)
 	end
-
-	uv.stop()
 end
 
 describe("AsyncFileReader", function()
 	describe("LoadFileContents", function()
 		it("should trigger FILE_REQUEST_FAILED if the given path is invalid", function()
-			assertEventTrigger(
-				function()
-					AsyncFileReader:LoadFileContents("does-not-exist")
-				end,
-				"FILE_REQUEST_FAILED",
-				{
-					fileSystemPath = "does-not-exist",
-					message = "ENOENT: no such file or directory: does-not-exist",
-				}
-			)
+			local function loadInvalidPath()
+				AsyncFileReader:LoadFileContents("does-not-exist")
+			end
+			local expectedPayload = {
+				fileSystemPath = "does-not-exist",
+				message = "ENOENT: no such file or directory: does-not-exist",
+			}
+			assertEvent(loadInvalidPath, "FILE_REQUEST_FAILED", expectedPayload)
 		end)
 
 		it("should trigger FILE_REQUEST_FAILED if the given path refers to a directory", function()
-			assertEventTrigger(
+			assertEvent(
 				function()
 					AsyncFileReader:LoadFileContents("Runtime")
 				end,
