3d0e1bec24b696c9694191a98de9b72cc49aaad3
RDW <duckwhale@mail.ru>
2024-02-22 13:55:31 +0100

    Update async-file-loading.lua

--- Benchmarks/async-file-loading.lua
+++ Benchmarks/async-file-loading.lua
@@ -1,9 +1,10 @@
 local AsyncFileReader = require("Runtime.API.FileSystem.AsyncFileReader") -- C_FileSystem.AsyncFileReader
 
 local console = require("console")
+local uv = require("uv")
 
 console.startTimer("Generating test fixtures")
-local SAMPLE_SIZE = 100000000
+local SAMPLE_SIZE = 1 --0000000
 local SMALL_FILE_PATH = path.join("Tests", "Fixtures", "test-small.txt")
 local LARGE_FILE_PATH = path.join("Tests", "Fixtures", "test-large.txt")
 local HUGE_FILE_PATH = path.join("Tests", "Fixtures", "test-huge.txt")
@@ -21,21 +22,56 @@ console.stopTimer("Generating test fixtures")
 math.randomseed(os.clock())
 local availableBenchmarks = {
 	function()
+		local label = "[ASYNC] Loading a small file repeatedly, many times"
+		console.startTimer(label)
+		for i = 1, SAMPLE_SIZE, 1 do
+			AsyncFileReader:LoadFileContents(SMALL_FILE_PATH)
+		end
+		uv.run()
+		console.stopTimer(label)
+	end,
+	function()
+		local label = "[ASYNC] Loading a large file repeatedly, many times"
+		console.startTimer(label)
+		for i = 1, SAMPLE_SIZE, 1 do
+			AsyncFileReader:LoadFileContents(LARGE_FILE_PATH)
+		end
+		uv.run()
+		console.stopTimer(label)
+	end,
+	function()
+		local label = "[ASYNC] Loading a huge file repeatedly, many times"
+		console.startTimer(label)
+		for i = 1, SAMPLE_SIZE, 1 do
+			AsyncFileReader:LoadFileContents(HUGE_FILE_PATH)
+		end
+		uv.run()
+		console.stopTimer(label)
+	end,
+	function()
 		local label = "[SYNC] Loading a small file repeatedly, many times"
 		console.startTimer(label)
 		for i = 1, SAMPLE_SIZE, 1 do
-			-- bitceil_lua(i)
+			C_FileSystem.ReadFile(SMALL_FILE_PATH)
 		end
 		console.stopTimer(label)
 	end,
 	function()
-		local label = "[ASYNC] Loading a small file repeatedly, many times"
+		local label = "[SYNC] Loading a large file repeatedly, many times"
 		console.startTimer(label)
 		for i = 1, SAMPLE_SIZE, 1 do
-			-- bit.ceil(i)
+			C_FileSystem.ReadFile(LARGE_FILE_PATH)
 		end
 		console.stopTimer(label)
 	end,
+	function()
+		local label = "[SYNC] Loading a huge file repeatedly, many times"
+		console.startTimer(label)
+		for i = 1, SAMPLE_SIZE, 1 do
+			C_FileSystem.ReadFile(HUGE_FILE_PATH)
+		end
+		console.stopTimer(label)
+	end
 }
 
 local function shuffle(tbl)
