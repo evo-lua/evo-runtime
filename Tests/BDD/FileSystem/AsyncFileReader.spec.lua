681782735e81ce5e762bd3da94e681b57f782d18
RDW <duckwhale@mail.ru>
2024-02-22 06:17:31 +0100

    Update AsyncFileReader.spec.lua

--- Tests/BDD/FileSystem/AsyncFileReader.spec.lua
+++ Tests/BDD/FileSystem/AsyncFileReader.spec.lua
@@ -19,13 +19,19 @@ local function assertEvent(functionToObserve, expectedEvent, expectedPayload, nu
 	for index = 1, numExpectedNotifications, 1 do
 		local event = observedEvents[index]
 		assertEquals(event.name, expectedEvent)
-		assertEquals(event.payload, expectedPayload)
+		for key, value in pairs(expectedPayload) do
+			assertEquals(event.payload[key], value)
+		end
 	end
 
 	if wasEventEnabled then
 		etrace.enable(expectedEvent)
 	end
 end
+
+local SMALL_TEST_FILE = "temp-small.txt"
+local FILE_CONTENTS_SMALL = string.rep("A", 1)--AsyncFileReader.CHUNK_SIZE_IN_BYTES - 1)
+C_FileSystem.WriteFile(SMALL_TEST_FILE, FILE_CONTENTS_SMALL)
 
 describe("AsyncFileReader", function()
 	describe("LoadFileContents", function()
@@ -50,5 +56,18 @@ describe("AsyncFileReader", function()
 			}
 			assertEvent(loadDirectory, "FILE_REQUEST_FAILED", expectedPayload)
 		end)
+
+		it("should trigger FILE_CONTENTS_AVAILABLE if the read doesn't require buffering", function()
+			local function loadSmallFile()
+				AsyncFileReader:LoadFileContents(SMALL_TEST_FILE)
+			end
+			local expectedPayload = {
+				fileSystemPath = SMALL_TEST_FILE,
+				fileContents = FILE_CONTENTS_SMALL,
+			}
+			assertEvent(loadSmallFile, "FILE_CONTENTS_AVAILABLE", expectedPayload)
+		end)
 	end)
 end)
+
+C_FileSystem.Delete(SMALL_TEST_FILE)
