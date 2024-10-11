b548d2f42e9dfa1be2c52f19b5ade26c29df8878
RDW <rdw-software@proton.me>
2024-10-11 17:53:01 +0200

    Create fs-test.lua

---
+++ fs-test.lua
@@ -0,0 +1,28 @@
+local uv = require("uv")
+
+local jit = require("jit")
+print(jit.version_num)
+-- jit.off()
+
+-- collectgarbage("stop")
+
+local handle
+handle = uv.fs_open("does-not-exist", "r", 438, function(err, fd) -- TODO req not handle
+	print(err, fd)
+	print(type(handle))
+	-- print(handle)
+	-- error("this is never executed")
+end)
+
+print(type(handle))
+print(handle)
+
+-- uv.run()
+
+uv.walk(function(_)
+	print(_)
+	-- print(_) -- uv_close: Assertion `!uv__is_closing(handle)' faile
+	-- error("this is never executed either")
+end)
+
+-- uv.run()
