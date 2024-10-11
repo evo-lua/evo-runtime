a46bfa85d9e83b801b9038d2770235e092f893d4
RDW <rdw-software@proton.me>
2024-10-11 17:52:56 +0200

    tbd use luajit from luv

--- deps/luv-unixbuild.sh
+++ deps/luv-unixbuild.sh
@@ -4,6 +4,7 @@ echo "Building target luv"
 
 SRC_DIR=deps/luvit/luv
 BUILD_DIR=$SRC_DIR/cmakebuild-unix
+LUAJIT_SRC_DIR=deps/luajit/src
 OUT_DIR=ninjabuild-unix
 
 cmake -S $SRC_DIR -B $BUILD_DIR -G Ninja -DBUILD_MODULE=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC_LIBS=ON -DCMAKE_C_COMPILER=gcc
@@ -11,4 +12,6 @@ cmake --build $BUILD_DIR --clean-first
 
 # Technically luv also builds LuaJIT (again), but it sometimes segfaults in JIT'ed code, so use the original instead to be safe
 cp $BUILD_DIR/libluv.a $OUT_DIR
+cp $BUILD_DIR/luajit $OUT_DIR
+cp $BUILD_DIR/libluajit.a $OUT_DIR
 cp $BUILD_DIR/deps/libuv/libuv.a $OUT_DIR
