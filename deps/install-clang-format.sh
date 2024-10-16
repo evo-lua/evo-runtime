e07100adb94f229fe2991d710a7adfc83b120650
RDW <rdw-software@proton.me>
2024-10-16 05:53:52 +0200

    Update install-clang-format.sh

--- deps/install-clang-format.sh
+++ deps/install-clang-format.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-REQUIRED_CLANG_FORMAT_VERSION="17"
 CLANG_FORMAT_DOWNLOAD_URL="https://apt.llvm.org/llvm.sh"
 
 echo "Downloading LLVM install script from $CLANG_FORMAT_DOWNLOAD_URL"
@@ -13,16 +12,16 @@ echo "You should inspect the contents of llvm.sh (e.g. via cat llvm.sh)"
 echo "Don't blindly run code that was downloaded from the internet"
 echo
 
-echo "The script will now attempt to install clang-format-$REQUIRED_CLANG_FORMAT_VERSION"
+echo "The script will now attempt to install clang-format-$EVO_AUTOFORMAT_VERSION"
 echo
 
 # This should prompt the user, giving them time to inspect llvm.sh if so desired (not in CI runs)
-sudo ./llvm.sh $REQUIRED_CLANG_FORMAT_VERSION
-sudo apt install clang-format-$REQUIRED_CLANG_FORMAT_VERSION
+sudo ./llvm.sh $EVO_AUTOFORMAT_VERSION
+sudo apt install clang-format-$EVO_AUTOFORMAT_VERSION
 echo
 
 echo "Cleanup: Removing llvm.sh"
 echo
 rm -rf llvm.sh
 
-echo $(clang-format-$REQUIRED_CLANG_FORMAT_VERSION --version)
+echo $(clang-format-$EVO_AUTOFORMAT_VERSION --version)
