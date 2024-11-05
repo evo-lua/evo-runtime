660d7e567423bd7a5a3c2b15b6134938298f6b4f
RDW <rdw-software@proton.me>
2024-11-06 00:22:08 +0100

    Update aes-encrypt.lua

--- Benchmarks/aes-encrypt.lua
+++ Benchmarks/aes-encrypt.lua
@@ -48,6 +48,57 @@ print(libcrypto)
 
 local ciphertextBuffer = buffer.new(1024)
 
+
+local function libcrypto_encrypt()
+
+end
+local function libcrypto_decrypt()
+-- 	int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
+-- 	unsigned char *iv, unsigned char *plaintext)
+-- {
+-- EVP_CIPHER_CTX *ctx;
+
+-- int len;
+
+-- int plaintext_len;
+
+-- /* Create and initialise the context */
+-- if(!(ctx = EVP_CIPHER_CTX_new()))
+-- handleErrors();
+
+-- /*
+-- * Initialise the decryption operation. IMPORTANT - ensure you use a key
+-- * and IV size appropriate for your cipher
+-- * In this example we are using 256 bit AES (i.e. a 256 bit key). The
+-- * IV size for *most* modes is the same as the block size. For AES this
+-- * is 128 bits
+-- */
+-- if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+-- handleErrors();
+
+-- /*
+-- * Provide the message to be decrypted, and obtain the plaintext output.
+-- * EVP_DecryptUpdate can be called multiple times if necessary.
+-- */
+-- if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+-- handleErrors();
+-- plaintext_len = len;
+
+-- /*
+-- * Finalise the decryption. Further plaintext bytes may be written at
+-- * this stage.
+-- */
+-- if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
+-- handleErrors();
+-- plaintext_len += len;
+
+-- /* Clean up */
+-- EVP_CIPHER_CTX_free(ctx);
+
+-- return plaintext_len;
+-- }
+end
+
 local function aes_encrypt_ffi(message)
 	local ctx = libcrypto.EVP_CIPHER_CTX_new()
 	-- if(!(ctx = EVP_CIPHER_CTX_new()))
