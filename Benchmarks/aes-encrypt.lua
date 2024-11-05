a5ea63695923c5cb652e9288e90a06817ee6bf85
RDW <rdw-software@proton.me>
2024-11-05 23:38:00 +0100

    Update aes-encrypt.lua

--- Benchmarks/aes-encrypt.lua
+++ Benchmarks/aes-encrypt.lua
@@ -1,10 +1,15 @@
-local bit = require("bit")
 local console = require("console")
+local cipher = require('openssl').cipher
 
-local SAMPLE_SIZE = 100
+local SAMPLE_SIZE = 100000
 
-local function aes_encrypt_lua(plainText)
-
+local function aes_encrypt_lua(message)
+	local method = "aes-128-ecb"
+	local key = string.rep("XY", 16)
+	local iv = string.rep(string.char(42), 32)
+	local encryptedMessage = cipher.encrypt(method, message, key, iv)
+	local decryptedMessage = cipher.decrypt(method, encryptedMessage, key, iv) -- TBD IV?
+	assert(decryptedMessage == message, "Decrypted message doesn't match the original plaintext")
 end
 
 local function aes_encrypt_ffi(plainText)
@@ -19,7 +24,7 @@ local availableBenchmarks = {
 		local label = "[Lua] AES-Encrypt via lua-openssl"
 		console.startTimer(label)
 		for i = 1, SAMPLE_SIZE, 1 do
-			aes_encrypt_lua("TBD")
+			aes_encrypt_lua("Hello world!")
 		end
 		console.stopTimer(label)
 	end,
