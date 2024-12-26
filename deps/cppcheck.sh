99d4c2d17d68b695795da709e171af43ca2740f2
RDW <rdw-software@proton.me>
2024-12-26 23:49:50 +0100

    CI: Exclude header files from cppcheck scans
    Those shouldn't be scanned directly; it'll lead to false positives (`unusedStructMember` warnings).

--- deps/cppcheck.sh
+++ deps/cppcheck.sh
@@ -7,6 +7,6 @@ INCLUDE_DIRS="-I Runtime -I Runtime/Bindings"
 SRC_DIRS="Runtime/*"
 IGNORE_LIST=".cppcheck"
 
-RELEVANT_FILES=$(find $SRC_DIRS -type f \( -name '*.c' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \))
+RELEVANT_FILES=$(find $SRC_DIRS -type f \( -name '*.c' -o -name '*.cpp' \))
 
 cppcheck --enable=all --error-exitcode=127 --suppressions-list=$IGNORE_LIST $INCLUDE_DIRS $RELEVANT_FILES
