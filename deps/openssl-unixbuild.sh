afc7c6d971c7fb4a12959bb1846f658a9d73e738
RDW <rdw-software@proton.me>
2025-01-15 15:21:31 +0100

    Deps: Update the OpenSSL build flags
    Adding some optional features that shouldn't have any downsides. Windows builds may be faster if using no-makedepend, but only when the build artifacts aren't cached.

--- deps/openssl-unixbuild.sh
+++ deps/openssl-unixbuild.sh
@@ -9,7 +9,12 @@ OPENSSL_DIR=deps/openssl/openssl
 
 cd $OPENSSL_DIR
 
-./config no-tests no-shared no-dso
+OPENSSL_STATIC_FLAGS="no-pinshared no-shared"
+OPENSSL_UNUSED_FEATURES="no-dso no-module no-ui-console" 
+OPENSSL_UNUSED_TARGETS="no-apps no-docs no-makedepend no-tests"
+OPENSSL_OPTIONAL_FEATURES="enable-ec_nistp_64_gcc_128 enable-ktls enable-tfo"
+OPENSSL_FEATURE_FLAGS="$OPENSSL_UNUSED_FEATURES $OPENSSL_OPTIONAL_FEATURES $OPENSSL_UNUSED_TARGETS"
+./config $OPENSSL_STATIC_FLAGS $OPENSSL_FEATURE_FLAGS
 
 make clean
 make -j $NUM_PARALLEL_JOBS
--- deps/openssl-windowsbuild.sh
+++ deps/openssl-windowsbuild.sh
@@ -10,7 +10,12 @@ OPENSSL_DIR=deps/openssl/openssl
 
 cd $OPENSSL_DIR
 
-perl Configure mingw64 no-tests no-shared no-dso
+OPENSSL_STATIC_FLAGS="no-pinshared no-shared"
+OPENSSL_UNUSED_FEATURES="no-dso no-module no-ui-console" 
+OPENSSL_UNUSED_TARGETS="no-apps no-docs no-makedepend no-tests"
+OPENSSL_OPTIONAL_FEATURES="enable-ec_nistp_64_gcc_128 enable-ktls enable-tfo"
+OPENSSL_FEATURE_FLAGS="$OPENSSL_UNUSED_FEATURES $OPENSSL_OPTIONAL_FEATURES $OPENSSL_UNUSED_TARGETS"
+perl Configure mingw64 $OPENSSL_STATIC_FLAGS $OPENSSL_FEATURE_FLAGS
 
 make clean
 make -j $NUM_PARALLEL_JOBS
