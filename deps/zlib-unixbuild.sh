a89043f15ae3f7bfd5f046c521fa1ea44d35a897
RDW <rdw-software@proton.me>
2025-01-17 12:49:08 +0100

    Deps: Enable building shared zlib (as a test fixture only)
    zlib is unlikely to ever be removed and quick to build, so there's no drawbacks to generating a shared library version as well.
    
    Although this variant won't be used by the runtime itself, it does come in handy for testing vfs.dlopen in a real-world scenario.

--- deps/zlib-unixbuild.sh
+++ deps/zlib-unixbuild.sh
@@ -6,8 +6,12 @@ OUT_DIR=ninjabuild-unix
 SRC_DIR=deps/madler/zlib
 BUILD_DIR=$SRC_DIR/cmakebuild-unix
 
-cmake -S $SRC_DIR -B $BUILD_DIR -G Ninja -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_COMPILER=gcc
+cmake -S $SRC_DIR -B $BUILD_DIR -G Ninja -DBUILD_SHARED_LIBS=ON -DCMAKE_C_COMPILER=gcc
 cmake --build $BUILD_DIR --clean-first
 
 cp $BUILD_DIR/libz.a $OUT_DIR/zlibstatic.a
 cp $BUILD_DIR/zconf.h $OUT_DIR
+
+# The shared library version is only used as a test fixture (somewhat arbitrary choice)
+TEST_APP_DIR=$(pwd)/Tests/Fixtures/dlopen-test-app
+cp $BUILD_DIR/libz.so $TEST_APP_DIR/zlib.so || cp $BUILD_DIR/libz.dylib $TEST_APP_DIR/zlib.so
