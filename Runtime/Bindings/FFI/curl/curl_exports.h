c3499556bf4ab66ade245406341c8ac80c50eca0
RDW <rdw-software@proton.me>
2025-02-07 04:17:15 +0100

    Update curl_exports.h

--- Runtime/Bindings/FFI/curl/curl_exports.h
+++ Runtime/Bindings/FFI/curl/curl_exports.h
@@ -4,13 +4,13 @@ struct static_curl_exports_table {
 	curl_version_info_data* (*curl_version_info)(CURLversion);
 
 	// urlapi.h
-	url_handle_t (*curl_url)(void);
-	void (*curl_url_cleanup)(url_handle_t);
-	CURLUcode (*curl_url_set)(url_handle_t url,
+	url_ptr_t (*curl_url)(void);
+	void (*curl_url_cleanup)(url_ptr_t);
+	CURLUcode (*curl_url_set)(url_ptr_t url,
 		CURLUPart part,
 		const char* content,
 		unsigned int flags);
-	CURLUcode (*curl_url_get)(const url_handle_t url,
+	CURLUcode (*curl_url_get)(url_cptr_t url,
 		CURLUPart part,
 		char** content,
 		unsigned int flags);
