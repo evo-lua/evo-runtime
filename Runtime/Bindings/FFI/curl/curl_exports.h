7b9fa6dc44f79592c977ee26e8b145264b9322df
RDW <rdw-software@proton.me>
2025-02-11 15:34:18 +0100

    Update curl_exports.h

--- Runtime/Bindings/FFI/curl/curl_exports.h
+++ Runtime/Bindings/FFI/curl/curl_exports.h
@@ -1,3 +1,12 @@
+typedef struct url_parts_t {
+	// TBD preallocation seems like overkill?
+} url_parts_t;
+
+typedef struct url_result_t {
+	int status;
+	const char* message;
+} url_result_t;
+
 struct static_curl_exports_table {
 	// curl.h
 	CURLversion CURLVERSION_NOW;
@@ -5,13 +14,17 @@ struct static_curl_exports_table {
 	void (*curl_free)(void*);
 	// urlapi.h
 	url_ptr_t (*curl_url)(void);
-	void (*curl_url_cleanup)(url_ptr_t);
-	CURLUcode (*curl_url_set)(url_ptr_t url,
-		CURLUPart part,
-		const char* content,
+	void (*curl_url_cleanup)(url_ptr_t handle);
+	url_ptr_t (*curl_url_dup)(url_cptr_t handle);
+	CURLUcode (*curl_url_get)(url_cptr_t handle,
+		CURLUPart what,
+		char** part,
 		unsigned int flags);
-	CURLUcode (*curl_url_get)(url_cptr_t url,
-		CURLUPart part,
-		char** content,
+	CURLUcode (*curl_url_set)(url_ptr_t handle,
+		CURLUPart what,
+		const char* part,
 		unsigned int flags);
+	const char* (*curl_url_strerror)(CURLUcode errno);
+	// custom
+	url_result_t* (*curl_decode_url)(const char* url); // TODO: Remove (doesn't seem much faster, needs alloc optimizations = deferred until later)
 };
