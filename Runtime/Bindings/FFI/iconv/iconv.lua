bb690db0a61ad145f87d7a3524d9baaee6ae1b82
RDW <rdw-software@proton.me>
2025-02-11 22:13:32 +0100

    Update iconv.lua

--- Runtime/Bindings/FFI/iconv/iconv.lua
+++ Runtime/Bindings/FFI/iconv/iconv.lua
@@ -20,18 +20,18 @@ typedef enum {
 	CharsetConversionFailure = 1,
 } iconv_result_t;
 
-typedef struct iconv_request_t {
-	iconv_progress_t input;
-	iconv_progress_t output;
-	iconv_t handle;
-} iconv_request_t;
-
 typedef struct iconv_progress_t {
 	char* buffer;
 	size_t length;
 	size_t remaining;
 	const char* encoding;
 } iconv_progress_t;
+
+typedef struct iconv_request_t {
+	iconv_progress_t input;
+	iconv_progress_t output;
+	iconv_t handle;
+} iconv_request_t;
 
 struct static_iconv_exports_table {
 	iconv_result_t (*iconv_convert)(iconv_request_t* conversion_details);
