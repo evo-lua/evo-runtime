5445b3b42da54c12ace63831cdbb6cedd7e120db
RDW <rdw-software@proton.me>
2025-02-11 22:06:20 +0100

    Update iconv.lua

--- Runtime/Bindings/FFI/iconv/iconv.lua
+++ Runtime/Bindings/FFI/iconv/iconv.lua
@@ -15,20 +15,29 @@ local iconv = {
 
 iconv.cdefs = [[
 typedef void* iconv_t;
-typedef struct iconv_result_t {
-	uint8_t status_code;
-	size_t num_bytes_written;
-	// const char* message;
+typedef enum {
+	CharsetConversionSuccess = 0,
+	CharsetConversionFailure = 1,
 } iconv_result_t;
 
+typedef struct iconv_request_t {
+	iconv_progress_t input;
+	iconv_progress_t output;
+	iconv_t handle;
+} iconv_request_t;
+
+typedef struct iconv_progress_t {
+	char* buffer;
+	size_t length;
+	size_t remaining;
+	const char* encoding;
+} iconv_progress_t;
+
 struct static_iconv_exports_table {
-	iconv_result_t (*iconv_convert)(char* input, size_t input_size, const char* input_encoding, const char* output_encoding, char* output, size_t output_size);
+	iconv_result_t (*iconv_convert)(iconv_request_t* conversion_details);
 	iconv_t (*iconv_open)(const char* input_encoding, const char* output_encoding);
 	int (*iconv_close)(iconv_t conversion_descriptor);
 	size_t (*iconv)(iconv_t conversion_descriptor, char** input, size_t* input_size, char** output, size_t* output_size);
-
-	// Shared constants
-	size_t CHARSET_CONVERSION_FAILED;
 };
 
 ]]
