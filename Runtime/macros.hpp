a94d5efeed10da4826be41a0aeade81e67843023
RDW <rdw-software@proton.me>
2025-02-16 15:53:52 +0100

    Runtime: Create a new macro for runtime asserts
    C++ has a bunch of tools for this kind of thing, but none seemed suitable at first glance. I'll revisit it later if needed.

--- Runtime/macros.hpp
+++ Runtime/macros.hpp
@@ -1,6 +1,15 @@
 #pragma once
 
+#include <cassert>
+
 #define EXPAND_AS_STRING(x) #x
 #define TOSTRING(x) EXPAND_AS_STRING(x)
 
 #define FROM_HERE __FILE__ ":" TOSTRING(__LINE__)
+
+// Silence [-Wunused-value] compiler warnings by adding (void)
+#ifdef NDEBUG
+#define ASSUME(condition, message) ((void)0)
+#else
+#define ASSUME(condition, failureMessage) assert((void(failureMessage), condition))
+#endif
