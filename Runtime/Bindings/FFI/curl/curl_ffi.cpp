0874ed15eb51ee0a4d2c56b35a874a7f6f52f09c
RDW <rdw-software@proton.me>
2025-02-16 18:37:19 +0100

    Runtime: Add libcurl's URL APIs to the FFI bindings

--- Runtime/Bindings/FFI/curl/curl_ffi.cpp
+++ Runtime/Bindings/FFI/curl/curl_ffi.cpp
@@ -4,9 +4,18 @@
 namespace curl_ffi {
 	void* getExportsTable() {
 		static struct static_curl_exports_table exports = {
-			// curl.h
+			// Exports from curl.h
 			.CURLVERSION_NOW = CURLVERSION_NOW,
 			.curl_version_info = curl_version_info,
+			.curl_free = curl_free,
+
+			// Exports from urlapi.h
+			.curl_url = curl_url,
+			.curl_url_cleanup = curl_url_cleanup,
+			.curl_url_dup = curl_url_dup,
+			.curl_url_get = curl_url_get,
+			.curl_url_set = curl_url_set,
+			.curl_url_strerror = curl_url_strerror,
 		};
 
 		return &exports;
